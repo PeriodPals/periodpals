0928065e3f0c30251c9d091cfa7b3ff2d171b216
charlie mangano <eva.mangano@epfl.ch>
2024-10-09 00:08:49 +0200

    feat: define basic supabase model for sign up
    Created class `com.android.periodpals.model.auth.AuthModelSupabase`, implementing interface `AuthModel`.
    It overrides every function from its interface.

---
+++ app/src/main/java/com/android/periodpals/model/auth/AuthModelSupabase.kt
@@ -0,0 +1,27 @@
+package com.android.periodpals.model.auth
+
+import io.github.jan.supabase.SupabaseClient
+import io.github.jan.supabase.auth.SignOutScope
+import io.github.jan.supabase.auth.auth
+import io.github.jan.supabase.auth.providers.builtin.Email
+
+class AuthModelSupabase(private val supabase: SupabaseClient) : AuthModel {
+
+  override suspend fun login(userEmail: String, userPassword: String) {
+    supabase.auth.signInWith(Email) {
+      email = userEmail
+      password = userPassword
+    }
+  }
+
+  override suspend fun register(userEmail: String, userPassword: String) {
+    supabase.auth.signUpWith(Email) {
+      email = userEmail
+      password = userPassword
+    }
+  }
+
+  override suspend fun logout() {
+    supabase.auth.signOut(SignOutScope.LOCAL)
+  }
+}
