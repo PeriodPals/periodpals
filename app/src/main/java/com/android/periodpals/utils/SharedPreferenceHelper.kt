c6db4f56b532639183d581180bf6dc73b43a0db1
charlie mangano <eva.mangano@epfl.ch>
2024-10-09 19:13:40 +0200

    feat: implement utility methods to save and retrieve data from Android's shared preferences
    The file used by `SharedPreferenceHelper` to store  and fetch the preferences is called `period_pals_prefs`.
    It saves preferences in a (key, value) structure.

---
+++ app/src/main/java/com/android/periodpals/utils/SharedPreferenceHelper.kt
@@ -0,0 +1,22 @@
+package com.android.periodpals.utils
+
+import android.content.Context
+
+class SharedPreferenceHelper(private val context: Context) {
+  companion object {
+    private const val PREFS_NAME = "period_pals_prefs"
+  }
+
+  fun saveStringData(key: String, value: String) {
+    val sharedPref = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+    with(sharedPref.edit()) {
+      putString(key, value)
+      apply()
+    }
+  }
+
+  fun getStringData(key: String): String? {
+    val sharedPref = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+    return sharedPref.getString(key, null)
+  }
+}
