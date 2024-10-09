6bd082703bfa3bedef4acecae2604620680f4ba3
charlie mangano <eva.mangano@epfl.ch>
2024-10-09 20:26:03 +0200

    feat: implement login with email, logout and check if user is logged in
    Added public functions to manage the authentication in the View Model:
     + `logInWithEmail` to log a user that already has a registered
     + `logOut` to log a user out
     + `isUserLoggedIn` to check whether or not the user is logged in

--- app/src/main/java/com/android/periodpals/model/auth/AuthViewModel.kt
+++ app/src/main/java/com/android/periodpals/model/auth/AuthViewModel.kt
@@ -19,7 +19,7 @@ class AuthViewModel(private val authModel: AuthModel) : ViewModel() {
 
   fun signUpWithEmail(context: Context, userEmail: String, userPassword: String) {
     viewModelScope.launch {
-      authModel.login(
+      authModel.register(
         email = userEmail,
         password = userPassword,
         onSuccess = {
@@ -35,11 +35,67 @@ class AuthViewModel(private val authModel: AuthModel) : ViewModel() {
     }
   }
 
-  private fun saveAccessToken(currentContext: Context) {
+  fun logInWithEmail(context: Context, userEmail: String, userPassword: String) {
+    viewModelScope.launch {
+      authModel.login(
+        email = userEmail,
+        password = userPassword,
+        onSuccess = {
+          saveAccessToken(context)
+          Log.d(TAG, "logInWithEmail: logged in successfully")
+          _userState.value = UserState.Success("Logged in successfully")
+        },
+        onFailure = { e: Exception ->
+          Log.d(TAG, "logInWithEmail: failed to log in: $e")
+          _userState.value = UserState.Error("Error: $e")
+        },
+      )
+    }
+  }
+
+  fun logOut() {
+    viewModelScope.launch {
+      authModel.logout(
+        onSuccess = {
+          Log.d(TAG, "logOut: logged out successfully")
+          _userState.value = UserState.Success("Logged out successfully")
+        },
+        onFailure = { e: Exception ->
+          Log.d(TAG, "logOut: failed to log out: $e")
+          _userState.value = UserState.Error("Error: $e")
+        },
+      )
+    }
+  }
+
+  fun isUserLoggedIn(context: Context) {
+    viewModelScope.launch {
+      authModel.isUserLoggedIn(
+        token = getTokens(context),
+        onSuccess = {
+          Log.d(TAG, "isUserLoggedIn: user is logged in")
+          saveAccessToken(context)
+          _userState.value = UserState.Success("User is logged in")
+        },
+        onFailure = {
+          Log.d(TAG, "isUserLoggedIn: user is not logged in")
+          _userState.value = UserState.Error("User is not logged in")
+          val loggedIn = false
+        },
+      )
+    }
+  }
+
+  private fun saveAccessToken(context: Context) {
     viewModelScope.launch {
       val accessToken = authModel.getAccessToken()
-      val sharedPreferences = SharedPreferenceHelper(currentContext)
+      val sharedPreferences = SharedPreferenceHelper(context)
       sharedPreferences.saveStringData("accessToken", accessToken)
     }
+  }
+
+  private fun getTokens(context: Context): String? {
+    val sharedPreferences = SharedPreferenceHelper(context)
+    return sharedPreferences.getStringData("accessToken")
   }
 }
