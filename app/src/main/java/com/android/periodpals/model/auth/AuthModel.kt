b537135fd54f161296d497a61f89e7a693cae056
charlie mangano <eva.mangano@epfl.ch>
2024-10-09 18:51:13 +0200

    feat: add `onSuccess` and `onFailure` lambdas to Model functions
    Changed the functions' signatures in `AuthModel` and `AuthModelSupabase`.

--- app/src/main/java/com/android/periodpals/model/auth/AuthModel.kt
+++ app/src/main/java/com/android/periodpals/model/auth/AuthModel.kt
@@ -2,9 +2,19 @@ package com.android.periodpals.model.auth
 
 interface AuthModel {
 
-  suspend fun login(userEmail: String, userPassword: String)
+  suspend fun login(
+    userEmail: String,
+    userPassword: String,
+    onSuccess: () -> Unit,
+    onFailure: (Error) -> Unit,
+  )
 
-  suspend fun register(userEmail: String, userPassword: String)
+  suspend fun register(
+    userEmail: String,
+    userPassword: String,
+    onSuccess: () -> Unit,
+    onFailure: (Error) -> Unit,
+  )
 
-  suspend fun logout()
+  suspend fun logout(onSuccess: () -> Unit, onFailure: (Error) -> Unit)
 }
--- app/src/main/java/com/android/periodpals/model/auth/AuthModelSupabase.kt
+++ app/src/main/java/com/android/periodpals/model/auth/AuthModelSupabase.kt
@@ -1,27 +1,50 @@
 package com.android.periodpals.model.auth
 
+import android.util.Log
 import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.auth.SignOutScope
 import io.github.jan.supabase.auth.auth
 import io.github.jan.supabase.auth.providers.builtin.Email
 
+private const val TAG = "AuthModelSupabase"
+
 class AuthModelSupabase(private val supabase: SupabaseClient) : AuthModel {
 
-  override suspend fun login(userEmail: String, userPassword: String) {
-    supabase.auth.signInWith(Email) {
-      email = userEmail
-      password = userPassword
+  override suspend fun login(
+    userEmail: String,
+    userPassword: String,
+    onSuccess: () -> Unit,
+    onFailure: (Error) -> Unit,
+  ) {
+    try {
+      supabase.auth.signInWith(Email) {
+        email = userEmail
+        password = userPassword
+      }
+      onSuccess()
+    } catch (e: Exception) {
+      Log.d(TAG, "Error: ${e.message}")
+      onFailure(Error(e.message))
     }
   }
 
-  override suspend fun register(userEmail: String, userPassword: String) {
+  override suspend fun register(
+    userEmail: String,
+    userPassword: String,
+    onSuccess: () -> Unit,
+    onFailure: (Error) -> Unit,
+  ) {
+    // TODO: try catch block
     supabase.auth.signUpWith(Email) {
       email = userEmail
       password = userPassword
     }
+    onSuccess()
   }
 
-  override suspend fun logout() {
+  override suspend fun logout(onSuccess: () -> Unit, onFailure: (Error) -> Unit) {
+    // TODO: try catch block
     supabase.auth.signOut(SignOutScope.LOCAL)
+    onSuccess()
   }
 }
