4922555d82e5798a2bdbfe2fa09ea47e3f0f2e62
France LU <france.lu@epfl.ch>
2024-10-10 13:57:26 +0200

    fix: remove preview of [Scaffold] component to prepare for pull request

--- app/src/main/java/com/android/periodpals/ui/navigation/BottomNavigationMenu.kt
+++ app/src/main/java/com/android/periodpals/ui/navigation/BottomNavigationMenu.kt
@@ -2,13 +2,11 @@ package com.android.periodpals.ui.navigation
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -16,7 +14,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -67,17 +64,4 @@ fun BottomNavigationMenu(
               onClick = { onTabSelect(tab) })
         }
       })
-}
-
-// TODO: Delete this screen
-@Preview
-@Composable
-fun ScreenToDelete() {
-  Scaffold(
-      topBar = { TopAppBar(title = "To Delete Screen", backButton = true, onBackButtonClick = {}) },
-      bottomBar = {
-        BottomNavigationMenu(
-            onTabSelect = {}, tabList = LIST_TOP_LEVEL_DESTINATION, selectedItem = "selectedItem")
-      },
-      content = { padding -> Text(text = "hmm", modifier = Modifier.padding(padding)) })
 }
