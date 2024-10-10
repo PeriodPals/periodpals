2acc890c63728ffa485e02d673c610df7f2e93a8
France LU <france.lu@epfl.ch>
2024-10-10 17:44:02 +0200

    fix: reformat code using ktfmt

--- app/src/test/java/com/android/periodpals/ui/navigation/NavigationActionsTest.kt
+++ app/src/test/java/com/android/periodpals/ui/navigation/NavigationActionsTest.kt
@@ -2,16 +2,10 @@ package com.android.periodpals.ui.navigation
 
 import androidx.navigation.NavDestination
 import androidx.navigation.NavHostController
-import androidx.navigation.NavOptionsBuilder
-import org.hamcrest.CoreMatchers.`is`
-import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
-import org.mockito.Mockito.`when`
-import org.mockito.kotlin.any
-import org.mockito.kotlin.eq
 
 class NavigationActionsTest {
 
@@ -26,23 +20,23 @@ class NavigationActionsTest {
     navigationActions = NavigationActions(navHostController)
   }
 
-  //TODO: Uncomment the tests
+  // TODO: Uncomment the tests
   /**
    * Test that the navigateTo function calls the navHostController with the correct route and
    * NavOptionsBuilder.
    */
   @Test
   fun navigateToTopLevelDestinations() {
-//    navigationActions.navigateTo(TopLevelDestinations.ALERT)
-//    verify(navHostController).navigate(eq(Route.ALERT), any<NavOptionsBuilder.() -> Unit>())
-//    navigationActions.navigateTo(TopLevelDestinations.NOTIF)
-//    verify(navHostController).navigate(eq(Route.NOTIF), any<NavOptionsBuilder.() -> Unit>())
-//    navigationActions.navigateTo(TopLevelDestinations.MAP)
-//    verify(navHostController).navigate(eq(Route.MAP), any<NavOptionsBuilder.() -> Unit>())
-//    navigationActions.navigateTo(TopLevelDestinations.TIMER)
-//    verify(navHostController).navigate(eq(Route.TIMER), any<NavOptionsBuilder.() -> Unit>())
-//    navigationActions.navigateTo(TopLevelDestinations.PROFILE)
-//    verify(navHostController).navigate(eq(Route.PROFILE), any<NavOptionsBuilder.() -> Unit>())
+    //    navigationActions.navigateTo(TopLevelDestinations.ALERT)
+    //    verify(navHostController).navigate(eq(Route.ALERT), any<NavOptionsBuilder.() -> Unit>())
+    //    navigationActions.navigateTo(TopLevelDestinations.NOTIF)
+    //    verify(navHostController).navigate(eq(Route.NOTIF), any<NavOptionsBuilder.() -> Unit>())
+    //    navigationActions.navigateTo(TopLevelDestinations.MAP)
+    //    verify(navHostController).navigate(eq(Route.MAP), any<NavOptionsBuilder.() -> Unit>())
+    //    navigationActions.navigateTo(TopLevelDestinations.TIMER)
+    //    verify(navHostController).navigate(eq(Route.TIMER), any<NavOptionsBuilder.() -> Unit>())
+    //    navigationActions.navigateTo(TopLevelDestinations.PROFILE)
+    //    verify(navHostController).navigate(eq(Route.PROFILE), any<NavOptionsBuilder.() -> Unit>())
   }
 
   /**
@@ -51,12 +45,12 @@ class NavigationActionsTest {
    */
   @Test
   fun navigateToAuthScreens() {
-//    navigationActions.navigateTo(Screen.AUTH)
-//    verify(navHostController).navigate(Screen.AUTH)
-//    navigationActions.navigateTo(Screen.REGISTER)
-//    verify(navHostController).navigate(Screen.REGISTER)
-//    navigationActions.navigateTo(Screen.CREATE_PROFILE)
-//    verify(navHostController).navigate(Screen.CREATE_PROFILE)
+    //    navigationActions.navigateTo(Screen.AUTH)
+    //    verify(navHostController).navigate(Screen.AUTH)
+    //    navigationActions.navigateTo(Screen.REGISTER)
+    //    verify(navHostController).navigate(Screen.REGISTER)
+    //    navigationActions.navigateTo(Screen.CREATE_PROFILE)
+    //    verify(navHostController).navigate(Screen.CREATE_PROFILE)
   }
 
   /**
@@ -65,18 +59,18 @@ class NavigationActionsTest {
    */
   @Test
   fun navigateToOtherScreens() {
-//    navigationActions.navigateTo(Screen.ALERT)
-//    verify(navHostController).navigate(Screen.ALERT)
-//    navigationActions.navigateTo(Screen.NOTIF)
-//    verify(navHostController).navigate(Screen.NOTIF)
-//    navigationActions.navigateTo(Screen.MAP)
-//    verify(navHostController).navigate(Screen.MAP)
-//    navigationActions.navigateTo(Screen.TIMER)
-//    verify(navHostController).navigate(Screen.TIMER)
-//    navigationActions.navigateTo(Screen.PROFILE)
-//    verify(navHostController).navigate(Screen.PROFILE)
-//    navigationActions.navigateTo(Screen.EDIT_PROFILE)
-//    verify(navHostController).navigate(Screen.EDIT_PROFILE)
+    //    navigationActions.navigateTo(Screen.ALERT)
+    //    verify(navHostController).navigate(Screen.ALERT)
+    //    navigationActions.navigateTo(Screen.NOTIF)
+    //    verify(navHostController).navigate(Screen.NOTIF)
+    //    navigationActions.navigateTo(Screen.MAP)
+    //    verify(navHostController).navigate(Screen.MAP)
+    //    navigationActions.navigateTo(Screen.TIMER)
+    //    verify(navHostController).navigate(Screen.TIMER)
+    //    navigationActions.navigateTo(Screen.PROFILE)
+    //    verify(navHostController).navigate(Screen.PROFILE)
+    //    navigationActions.navigateTo(Screen.EDIT_PROFILE)
+    //    verify(navHostController).navigate(Screen.EDIT_PROFILE)
   }
 
   /** Test that the goBack function calls the navHostController to pop the back stack. */
@@ -93,16 +87,16 @@ class NavigationActionsTest {
   @Test
   fun currentRouteWorksWithDestination() {
     // Mock the current destination to be ALERT
-//    `when`(navHostController.currentDestination).thenReturn(navigationDestination)
-//    `when`(navigationDestination.route).thenReturn(Route.ALERT)
-//    assertThat(navigationActions.currentRoute(), `is`(Route.ALERT))
-//
-//    // Mock the current destination to be MAP
-//    `when`(navigationDestination.route).thenReturn(Route.MAP)
-//    assertThat(navigationActions.currentRoute(), `is`(Route.MAP))
-//
-//    // Mock the current destination to be null
-//    `when`(navHostController.currentDestination).thenReturn(null)
-//    assertThat(navigationActions.currentRoute(), `is`(""))
+    //    `when`(navHostController.currentDestination).thenReturn(navigationDestination)
+    //    `when`(navigationDestination.route).thenReturn(Route.ALERT)
+    //    assertThat(navigationActions.currentRoute(), `is`(Route.ALERT))
+    //
+    //    // Mock the current destination to be MAP
+    //    `when`(navigationDestination.route).thenReturn(Route.MAP)
+    //    assertThat(navigationActions.currentRoute(), `is`(Route.MAP))
+    //
+    //    // Mock the current destination to be null
+    //    `when`(navHostController.currentDestination).thenReturn(null)
+    //    assertThat(navigationActions.currentRoute(), `is`(""))
   }
 }
