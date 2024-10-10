05c253e9ac9201b7b438e71c189ca998ba7d1c99
Bruno Lazarini Sigg <bruno.lazarinisigg@epfl.ch>
2024-10-10 18:04:51 +0200

    refactor: restore MainActivity.kt how it was in the main to avoid merge issues and deleted provisory resource image

--- app/src/main/java/com/android/periodpals/MainActivity.kt
+++ app/src/main/java/com/android/periodpals/MainActivity.kt
@@ -34,7 +34,7 @@ class MainActivity : ComponentActivity() {
       PeriodPalsAppTheme {
         // A surface container using the 'background' color from the theme
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-          AlertListScreen()
+            CountriesList()
         }
       }
     }
