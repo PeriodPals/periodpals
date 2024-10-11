01e90dc7409cd5ae2475f4684a7bffeb36bde206
Bruno Lazarini Sigg <bruno.lazarinisigg@epfl.ch>
2024-10-11 16:59:49 +0200

    refactor: KTFmt format

--- app/src/main/java/com/android/periodpals/ui/alertsList/AlertsList.kt
+++ app/src/main/java/com/android/periodpals/ui/alertsList/AlertsList.kt
@@ -15,7 +15,6 @@ import androidx.compose.material.icons.outlined.Call
 import androidx.compose.material.icons.outlined.Warning
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
