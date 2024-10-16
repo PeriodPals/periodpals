149dbf79adb5f780f5df9f9f0512a293035012d2
taghizadlaura <laura.taghizad@epfl.ch>
2024-10-16 10:39:05 +0200

    style: run ktfmt

--- app/src/androidTest/java/com/android/periodpals/ui/profile/ProfileScreenTest.kt
+++ app/src/androidTest/java/com/android/periodpals/ui/profile/ProfileScreenTest.kt
@@ -8,23 +8,22 @@ import org.junit.Rule
 import org.junit.Test
 
 class ProfileScreenTest {
-    @get:Rule
-    val composeTestRule = createComposeRule()
+  @get:Rule val composeTestRule = createComposeRule()
 
-    @Test
-    fun displayAllComponents() {
-        composeTestRule.setContent { ProfileScreen() }
-        composeTestRule.onNodeWithTag("profileAvatar").assertIsDisplayed()
-        composeTestRule.onNodeWithTag("profileName").assertIsDisplayed()
-        composeTestRule.onNodeWithTag("Description").assertIsDisplayed()
-        composeTestRule.onNodeWithTag("reviewOne").assertIsDisplayed()
-        composeTestRule.onNodeWithTag("reviewTwo").assertIsDisplayed()
-    }
+  @Test
+  fun displayAllComponents() {
+    composeTestRule.setContent { ProfileScreen() }
+    composeTestRule.onNodeWithTag("profileAvatar").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("profileName").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("Description").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("reviewOne").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("reviewTwo").assertIsDisplayed()
+  }
 
-    @Test
-    fun profileScreen_hasCorrectContent() {
-        composeTestRule.setContent { ProfileScreen() }
-        composeTestRule.onNodeWithTag("profileName").assertTextEquals("Name")
-        composeTestRule.onNodeWithTag("Description").assertTextEquals("Description")
-    }
+  @Test
+  fun profileScreen_hasCorrectContent() {
+    composeTestRule.setContent { ProfileScreen() }
+    composeTestRule.onNodeWithTag("profileName").assertTextEquals("Name")
+    composeTestRule.onNodeWithTag("Description").assertTextEquals("Description")
+  }
 }
