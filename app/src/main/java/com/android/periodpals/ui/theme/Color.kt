5d6b586dade4ff670ba9aab9419e13ee4c1d150f
Charlie <95096383+charliemangano@users.noreply.github.com>
2024-10-16 17:29:09 +0200

    Revert "feat: add app logo"
    This reverts commit 1682fc48aa1c136efefbce43b6122e8669d0bdfd.

--- app/src/main/java/com/android/periodpals/ui/theme/Color.kt
+++ app/src/main/java/com/android/periodpals/ui/theme/Color.kt
@@ -10,7 +10,10 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-/* Light theme, still in development and thus not in use */
+
+/* Color for a future theme. Still in development. */
+
+/* Light theme */
 val primaryLight = Color(0xFF415F91)
 val onPrimaryLight = Color(0xFFFFFFFF)
 val primaryContainerLight = Color(0xFFD6E3FF)
@@ -48,7 +51,7 @@ val surfaceContainerHighLight = Color(0xFFE7E8EE)
 val surfaceContainerHighestLight = Color(0xFFE2E2E9)
 
 
-/* Dark theme, still in development and thus not in use */
+/* Dark theme */
 val primaryDark = Color(0xFFAAC7FF)
 val onPrimaryDark = Color(0xFF0A305F)
 val primaryContainerDark = Color(0xFF284777)
