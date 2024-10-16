a7decdcff2fedcf9e4b6a40a0b9cf3750369adbe
taghizadlaura <laura.taghizad@epfl.ch>
2024-10-16 10:34:19 +0200

    fix: change tests with assertTextEquals

--- app/src/androidTest/java/com/android/periodpals/ui/profile/ProfileScreenTest.kt
+++ app/src/androidTest/java/com/android/periodpals/ui/profile/ProfileScreenTest.kt
@@ -1,28 +1,30 @@
 package com.android.periodpals.ui.profile
 
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import org.junit.Rule
 import org.junit.Test
 
 class ProfileScreenTest {
-  @get:Rule val composeTestRule = createComposeRule()
+    @get:Rule
+    val composeTestRule = createComposeRule()
 
-  @Test
-  fun displayAllComponents() {
-    composeTestRule.setContent { ProfileScreen() }
-    composeTestRule.onNodeWithTag("profileAvatar").assertIsDisplayed()
-    composeTestRule.onNodeWithTag("profileName").assertIsDisplayed()
-    composeTestRule.onNodeWithTag("Description").assertIsDisplayed()
-    composeTestRule.onNodeWithTag("reviewOne").assertIsDisplayed()
-    composeTestRule.onNodeWithTag("reviewTwo").assertIsDisplayed()
-  }
+    @Test
+    fun displayAllComponents() {
+        composeTestRule.setContent { ProfileScreen() }
+        composeTestRule.onNodeWithTag("profileAvatar").assertIsDisplayed()
+        composeTestRule.onNodeWithTag("profileName").assertIsDisplayed()
+        composeTestRule.onNodeWithTag("Description").assertIsDisplayed()
+        composeTestRule.onNodeWithTag("reviewOne").assertIsDisplayed()
+        composeTestRule.onNodeWithTag("reviewTwo").assertIsDisplayed()
+    }
 
-  @Test
-  fun profileScreen_hasCorrectContent() {
-    composeTestRule.setContent { ProfileScreen() }
-    composeTestRule.onNodeWithTag("profileName").assertExists("Displayed Name")
-    composeTestRule.onNodeWithTag("Description").assertExists("Description")
-  }
+    @Test
+    fun profileScreen_hasCorrectContent() {
+        composeTestRule.setContent { ProfileScreen() }
+        composeTestRule.onNodeWithTag("profileName").assertTextEquals("Name")
+        composeTestRule.onNodeWithTag("Description").assertTextEquals("Description")
+    }
 }
