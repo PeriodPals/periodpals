759c0a1f39176758746772d0d0b42c0d461e6a80
Alonso <alonso.coaguila@epfl.ch>
2024-10-17 23:19:18 +0200

    fix: merge from main

--- app/src/main/java/com/android/periodpals/MainActivity.kt
+++ app/src/main/java/com/android/periodpals/MainActivity.kt
@@ -13,16 +13,11 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.core.app.ActivityCompat
