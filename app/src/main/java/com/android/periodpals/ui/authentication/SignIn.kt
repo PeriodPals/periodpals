07cc45b34eacf92a6e1e7ff9ba3ad2d4e1098e47
charlie mangano <eva.mangano@epfl.ch>
2024-10-18 05:11:11 +0200

    feat: integrate Supabase signIn into the `SignInScreen`

--- app/src/main/java/com/android/periodpals/ui/authentication/SignIn.kt
+++ app/src/main/java/com/android/periodpals/ui/authentication/SignIn.kt
@@ -23,6 +23,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -38,9 +39,10 @@ import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.android.periodpals.R
+import com.android.periodpals.model.auth.AuthViewModel
+import com.android.periodpals.model.user.UserAuthState
 import com.android.periodpals.ui.components.AuthButton
 import com.android.periodpals.ui.components.AuthEmailInput
 import com.android.periodpals.ui.components.AuthInstruction
@@ -49,14 +51,16 @@ import com.android.periodpals.ui.components.AuthSecondInstruction
 import com.android.periodpals.ui.components.AuthWelcomeText
 import com.android.periodpals.ui.components.ErrorText
 import com.android.periodpals.ui.components.GradedBackground
+import com.android.periodpals.ui.navigation.NavigationActions
+import com.android.periodpals.ui.navigation.Screen
 import com.android.periodpals.ui.theme.Pink40
 import com.android.periodpals.ui.theme.Purple80
 import com.android.periodpals.ui.theme.PurpleGrey80
 
-@Preview
 @Composable
-fun SignInScreen() {
+fun SignInScreen(authViewModel: AuthViewModel, navigationActions: NavigationActions) {
   val context = LocalContext.current
+  val userState: UserAuthState by authViewModel.userAuthState
 
   var email by remember { mutableStateOf("") }
   var password by remember { mutableStateOf("") }
@@ -65,6 +69,9 @@ fun SignInScreen() {
   var passwordErrorMessage by remember { mutableStateOf("") }
 
   var passwordVisible by remember { mutableStateOf(false) }
+
+  var currentUserAuthState by remember { mutableStateOf("") }
+  LaunchedEffect(Unit) { authViewModel.isUserLoggedIn(context) }
 
   // Screen
   Scaffold(
@@ -80,7 +87,10 @@ fun SignInScreen() {
         ) {
           // Welcome text
           AuthWelcomeText(
-              text = "Welcome to PeriodPals", color = Color.Black, testTag = "signInTitle")
+              text = "Welcome to PeriodPals",
+              color = Color.Black,
+              testTag = "signInTitle",
+          )
 
           // Rectangle with login fields and button
           Box(
@@ -91,69 +101,72 @@ fun SignInScreen() {
                       .padding(24.dp)) {
                 Column(
                     horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.spacedBy(12.dp, Alignment.CenterVertically)) {
-                      // Sign in instruction
-                      AuthInstruction(
-                          text = "Sign in to your account", testTag = "signInInstruction")
+                    verticalArrangement = Arrangement.spacedBy(12.dp, Alignment.CenterVertically),
+                ) {
+                  // Sign in instruction
+                  AuthInstruction(text = "Sign in to your account", testTag = "signInInstruction")
 
-                      // Email input and error message
-                      AuthEmailInput(
-                          email = email, onEmailChange = { email = it }, testTag = "signInEmail")
-                      if (emailErrorMessage.isNotEmpty()) {
-                        ErrorText(emailErrorMessage, "signInEmailError")
-                      }
+                  // Email input and error message
+                  AuthEmailInput(
+                      email = email, onEmailChange = { email = it }, testTag = "signInEmail")
+                  if (emailErrorMessage.isNotEmpty()) {
+                    ErrorText(emailErrorMessage, "signInEmailError")
+                  }
 
-                      // Password input and error message
-                      AuthPasswordInput(
-                          password = password,
-                          onPasswordChange = { password = it },
-                          passwordVisible = passwordVisible,
-                          onPasswordVisibilityChange = { passwordVisible = !passwordVisible },
-                          testTag = "signInPassword",
-                          visibilityTestTag = "signInPasswordVisibility")
-                      if (passwordErrorMessage.isNotEmpty()) {
-                        ErrorText(passwordErrorMessage, "signInPasswordError")
-                      }
+                  // Password input and error message
+                  AuthPasswordInput(
+                      password = password,
+                      onPasswordChange = { password = it },
+                      passwordVisible = passwordVisible,
+                      onPasswordVisibilityChange = { passwordVisible = !passwordVisible },
+                      testTag = "signInPassword",
+                      visibilityTestTag = "signInPasswordVisibility",
+                  )
+                  if (passwordErrorMessage.isNotEmpty()) {
+                    ErrorText(passwordErrorMessage, "signInPasswordError")
+                  }
 
-                      // Sign in button
-                      AuthButton(
-                          text = "Sign in",
-                          onClick = {
-                            emailErrorMessage = validateEmail(email)
-                            passwordErrorMessage = validatePassword(password)
+                  // Sign in button
+                  AuthButton(
+                      text = "Sign in",
+                      onClick = {
+                        emailErrorMessage = validateEmail(email)
+                        passwordErrorMessage = validatePassword(password)
 
-                            if (emailErrorMessage.isEmpty() && passwordErrorMessage.isEmpty()) {
-                              // TODO: Implement email and password login logic
-                              val loginSuccess = true
-                              if (loginSuccess) {
-                                // with supabase
-                                Toast.makeText(context, "Login Successful", Toast.LENGTH_SHORT)
-                                    .show()
-                              } else {
-                                Toast.makeText(context, "Login Failed", Toast.LENGTH_SHORT).show()
-                              }
-                            } else {
-                              Toast.makeText(
-                                      context, "Invalid email or password.", Toast.LENGTH_SHORT)
-                                  .show()
-                            }
-                          },
-                          testTag = "signInButton")
+                        if (emailErrorMessage.isEmpty() && passwordErrorMessage.isEmpty()) {
+                          authViewModel.logInWithEmail(context, email, password)
+                          authViewModel.isUserLoggedIn(context)
+                          val loginSuccess = userState is UserAuthState.Success
+                          if (loginSuccess) {
+                            Toast.makeText(context, "Login Successful", Toast.LENGTH_SHORT).show()
+                            navigationActions.navigateTo(Screen.PROFILE)
+                          } else {
+                            Toast.makeText(context, "Login Failed", Toast.LENGTH_SHORT).show()
+                          }
+                        } else {
+                          Toast.makeText(context, "Invalid email or password.", Toast.LENGTH_SHORT)
+                              .show()
+                        }
+                      },
+                      testTag = "signInButton",
+                  )
 
-                      // Or continue with text
-                      AuthSecondInstruction(text = "Or continue with", testTag = "signInOrText")
+                  // Or continue with text
+                  AuthSecondInstruction(text = "Or continue with", testTag = "signInOrText")
 
-                      // Google sign in button
-                      GoogleButton(
-                          onClick = {
-                            Toast.makeText(
-                                    context,
-                                    "Use other login method for now, thanks!",
-                                    Toast.LENGTH_SHORT)
-                                .show()
-                          },
-                          testTag = "signInGoogleButton")
-                    }
+                  // Google sign in button
+                  GoogleButton(
+                      onClick = {
+                        Toast.makeText(
+                                context,
+                                "Use other login method for now, thanks!",
+                                Toast.LENGTH_SHORT,
+                            )
+                            .show()
+                      },
+                      testTag = "signInGoogleButton",
+                  )
+                }
               }
           // Not registered yet? Sign up here!
           val annotatedText = buildAnnotatedString {
@@ -169,14 +182,12 @@ fun SignInScreen() {
                 annotatedText
                     .getStringAnnotations(tag = "SignUp", start = offset, end = offset)
                     .firstOrNull()
-                    ?.let {
-                      /* TODO: Implement navigation action */
-                      Toast.makeText(context, "Yay! I'm waiting for navigation", Toast.LENGTH_SHORT)
-                          .show()
-                    }
-              })
+                    ?.let { navigationActions.navigateTo(Screen.SIGN_UP) }
+              },
+          )
         }
-      })
+      },
+  )
 }
 
 /** Validates the email and returns an error message if the email is invalid. */
@@ -210,20 +221,24 @@ fun GoogleButton(onClick: () -> Unit, modifier: Modifier = Modifier, testTag: St
       onClick = onClick,
       colors = ButtonDefaults.buttonColors(containerColor = Color.White),
       shape = RoundedCornerShape(50),
-      border = BorderStroke(1.dp, Color.LightGray)) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center) {
-              Image(
-                  painter = painterResource(id = R.drawable.google_logo),
-                  contentDescription = "Google Logo",
-                  modifier = Modifier.size(24.dp))
-              Spacer(modifier = Modifier.size(8.dp))
-              Text(
-                  text = "Sign in with Google",
-                  color = Color.Black,
-                  fontWeight = FontWeight.Medium,
-                  style = MaterialTheme.typography.bodyMedium)
-            }
-      }
+      border = BorderStroke(1.dp, Color.LightGray),
+  ) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center,
+    ) {
+      Image(
+          painter = painterResource(id = R.drawable.google_logo),
+          contentDescription = "Google Logo",
+          modifier = Modifier.size(24.dp),
+      )
+      Spacer(modifier = Modifier.size(8.dp))
+      Text(
+          text = "Sign in with Google",
+          color = Color.Black,
+          fontWeight = FontWeight.Medium,
+          style = MaterialTheme.typography.bodyMedium,
+      )
+    }
+  }
 }
