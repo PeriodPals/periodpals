35752f76e0ae8ad383c035a5bd3112ec56208135
Bruno Lazarini Sigg <bruno.lazarinisigg@epfl.ch>
2024-10-28 23:52:21 +0100

    style: optimize imports, ktfmt

--- app/src/main/java/com/android/periodpals/services/LocationService.kt
+++ app/src/main/java/com/android/periodpals/services/LocationService.kt
@@ -1,14 +1,8 @@
 package com.android.periodpals.services
 
-import kotlinx.coroutines.flow.StateFlow
-
-/**
- * A service that provides the user location.
- */
+/** A service that provides the user location. */
 interface LocationService {
 
-  /**
-   * Requests the user for permission to access their location.
-   */
+  /** Requests the user for permission to access their location. */
   fun requestUserPermissionForLocation()
 }
--- app/src/main/java/com/android/periodpals/services/LocationServiceImpl.kt
+++ app/src/main/java/com/android/periodpals/services/LocationServiceImpl.kt
@@ -1,49 +1,45 @@
 package com.android.periodpals.services
 
-import androidx.activity.ComponentActivity
-import androidx.activity.result.ActivityResultLauncher
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import android.Manifest
 import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 
 /**
  * An implementation of the location service.
+ *
  * @param activity The screen from which the location service is being launched from.
  */
-class LocationServiceImpl(
-  private val activity: ComponentActivity
-) : LocationService {
+class LocationServiceImpl(private val activity: ComponentActivity) : LocationService {
 
   // Initialize the ActivityResultLauncher which handles the permission request process
   private val requestPermissionLauncher: ActivityResultLauncher<Array<String>> =
-    activity.registerForActivityResult(
-      ActivityResultContracts.RequestMultiplePermissions()
-    ) { permissions -> // callback after the user responds to the permission dialog
-      when {
-        permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+      activity.registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+          permissions -> // callback after the user responds to the permission dialog
+        when {
           // Precise location access granted
-          Toast.makeText(activity, "Precise location access granted", Toast.LENGTH_LONG).show()
-        }
-        permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+          permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+            Toast.makeText(activity, "Precise location access granted", Toast.LENGTH_LONG).show()
+          }
+
           // Only approximate location access granted
-          Toast.makeText(activity, "Only approximate location access granted", Toast.LENGTH_LONG).show()
-        }
-        else -> {
+          permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+            Toast.makeText(activity, "Only approximate location access granted", Toast.LENGTH_LONG)
+                .show()
+          }
+
           // No location access granted
-          Toast.makeText(activity, "No location access granted", Toast.LENGTH_LONG).show()
+          else -> {
+            Toast.makeText(activity, "No location access granted", Toast.LENGTH_LONG).show()
+          }
         }
       }
-    }
 
   override fun requestUserPermissionForLocation() {
     // Launch the permission request
     requestPermissionLauncher.launch(
-      arrayOf(
-        Manifest.permission.ACCESS_FINE_LOCATION,
-        Manifest.permission.ACCESS_COARSE_LOCATION)
-    )
+        arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION))
   }
 }
