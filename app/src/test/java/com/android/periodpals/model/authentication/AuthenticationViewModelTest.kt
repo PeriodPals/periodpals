1c6dc9324809b1d300316701de8ec30e94fa644f
charlie mangano <eva.mangano@epfl.ch>
2024-10-29 20:41:02 +0100

    style: format using ktfmt for readability

--- app/src/test/java/com/android/periodpals/model/authentication/AuthenticationViewModelTest.kt
+++ app/src/test/java/com/android/periodpals/model/authentication/AuthenticationViewModelTest.kt
@@ -34,133 +34,133 @@ class AuthenticationViewModelTest {
   @Test
   fun `signUpWithEmail success`() = runBlocking {
     doAnswer { inv -> (inv.getArgument<() -> Unit>(2))() }
-      .`when`(authModel)
-      .register(any<String>(), any<String>(), any<() -> Unit>(), any<(Exception) -> Unit>())
+        .`when`(authModel)
+        .register(any<String>(), any<String>(), any<() -> Unit>(), any<(Exception) -> Unit>())
 
     authenticationViewModel.signUpWithEmail(userEmail = email, userPassword = password)
 
     val result =
-      when (authenticationViewModel.userAuthState.value) {
-        is UserAuthState.Success -> true
-        else -> false
-      }
+        when (authenticationViewModel.userAuthState.value) {
+          is UserAuthState.Success -> true
+          else -> false
+        }
     assert(result)
   }
 
   @Test
   fun `signUpWithEmail failure`() = runBlocking {
     doAnswer { inv -> (inv.getArgument<(Exception) -> Unit>(3))(Exception("signup failure")) }
-      .`when`(authModel)
-      .register(any<String>(), any<String>(), any<() -> Unit>(), any<(Exception) -> Unit>())
+        .`when`(authModel)
+        .register(any<String>(), any<String>(), any<() -> Unit>(), any<(Exception) -> Unit>())
 
     authenticationViewModel.signUpWithEmail(userEmail = email, userPassword = password)
 
     val result =
-      when (authenticationViewModel.userAuthState.value) {
-        is UserAuthState.Error -> true
-        else -> false
-      }
+        when (authenticationViewModel.userAuthState.value) {
+          is UserAuthState.Error -> true
+          else -> false
+        }
     assert(result)
   }
 
   @Test
   fun `signInWithEmail success`() = runBlocking {
     doAnswer { inv -> inv.getArgument<() -> Unit>(2)() }
-      .`when`(authModel)
-      .login(any<String>(), any<String>(), any<() -> Unit>(), any<(Exception) -> Unit>())
+        .`when`(authModel)
+        .login(any<String>(), any<String>(), any<() -> Unit>(), any<(Exception) -> Unit>())
 
     authenticationViewModel.logInWithEmail(userEmail = email, userPassword = password)
 
     val result =
-      when (authenticationViewModel.userAuthState.value) {
-        is UserAuthState.Success -> true
-        else -> false
-      }
+        when (authenticationViewModel.userAuthState.value) {
+          is UserAuthState.Success -> true
+          else -> false
+        }
     assert(result)
   }
 
   @Test
   fun `signInWithEmail failure`() = runBlocking {
     doAnswer { inv ->
-        val onFailure = inv.getArgument<(Exception) -> Unit>(3)
-        onFailure(Exception("sign in failure"))
-      }
-      .`when`(authModel)
-      .login(any<String>(), any<String>(), any<() -> Unit>(), any<(Exception) -> Unit>())
+          val onFailure = inv.getArgument<(Exception) -> Unit>(3)
+          onFailure(Exception("sign in failure"))
+        }
+        .`when`(authModel)
+        .login(any<String>(), any<String>(), any<() -> Unit>(), any<(Exception) -> Unit>())
 
     authenticationViewModel.logInWithEmail(userEmail = email, userPassword = password)
 
     val result =
-      when (authenticationViewModel.userAuthState.value) {
-        is UserAuthState.Success -> false
-        is UserAuthState.Error -> true
-        is UserAuthState.Loading -> false
-        else -> false
-      }
+        when (authenticationViewModel.userAuthState.value) {
+          is UserAuthState.Success -> false
+          is UserAuthState.Error -> true
+          is UserAuthState.Loading -> false
+          else -> false
+        }
     assert(result)
   }
 
   @Test
   fun `logOut success`() = runBlocking {
     doAnswer { inv -> inv.getArgument<() -> Unit>(0)() }
-      .`when`(authModel)
-      .logout(any<() -> Unit>(), any<(Exception) -> Unit>())
+        .`when`(authModel)
+        .logout(any<() -> Unit>(), any<(Exception) -> Unit>())
 
     authenticationViewModel.logOut()
 
     val result =
-      when (authenticationViewModel.userAuthState.value) {
-        is UserAuthState.Success -> true
-        else -> false
-      }
+        when (authenticationViewModel.userAuthState.value) {
+          is UserAuthState.Success -> true
+          else -> false
+        }
     assert(result)
   }
 
   @Test
   fun `logOut failure`() = runBlocking {
     doAnswer { inv -> inv.getArgument<(Exception) -> Unit>(1)(Exception("logout failure")) }
-      .`when`(authModel)
-      .logout(any<() -> Unit>(), any<(Exception) -> Unit>())
+        .`when`(authModel)
+        .logout(any<() -> Unit>(), any<(Exception) -> Unit>())
 
     authenticationViewModel.logOut()
 
     val result =
-      when (authenticationViewModel.userAuthState.value) {
-        is UserAuthState.Error -> true
-        else -> false
-      }
+        when (authenticationViewModel.userAuthState.value) {
+          is UserAuthState.Error -> true
+          else -> false
+        }
     assert(result)
   }
 
   @Test
   fun `isUserLoggedIn success`() = runBlocking {
     doAnswer { inv -> inv.getArgument<() -> Unit>(0)() }
-      .`when`(authModel)
-      .isUserLoggedIn(any<() -> Unit>(), any<(Exception) -> Unit>())
+        .`when`(authModel)
+        .isUserLoggedIn(any<() -> Unit>(), any<(Exception) -> Unit>())
 
     authenticationViewModel.isUserLoggedIn()
 
     val result =
-      when (authenticationViewModel.userAuthState.value) {
-        is UserAuthState.Success -> true
-        else -> false
-      }
+        when (authenticationViewModel.userAuthState.value) {
+          is UserAuthState.Success -> true
+          else -> false
+        }
     assert(result)
   }
 
   @Test
   fun `isUserLoggedIn failure`() = runBlocking {
     doAnswer { inv -> inv.getArgument<(Exception) -> Unit>(1)(Exception("user not logged in")) }
-      .`when`(authModel)
-      .isUserLoggedIn(any<() -> Unit>(), any<(Exception) -> Unit>())
+        .`when`(authModel)
+        .isUserLoggedIn(any<() -> Unit>(), any<(Exception) -> Unit>())
 
     authenticationViewModel.isUserLoggedIn()
 
     val result =
-      when (authenticationViewModel.userAuthState.value) {
-        is UserAuthState.Error -> true
-        else -> false
-      }
+        when (authenticationViewModel.userAuthState.value) {
+          is UserAuthState.Error -> true
+          else -> false
+        }
     assert(result)
   }
 }
