e2a26b91eb5fc72de104f0f4808830cc46bdb7a6
Bruno Lazarini Sigg <bruno.lazarinisigg@epfl.ch>
2024-10-29 19:51:00 +0100

    feat: add LocationAccessType and a StateFlow that exposes the type of location acces granted

--- app/src/main/java/com/android/periodpals/services/LocationServiceImpl.kt
+++ app/src/main/java/com/android/periodpals/services/LocationServiceImpl.kt
@@ -5,6 +5,15 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+/**
+ * Enum representing the type of location tha the user granted.
+ */
+enum class LocationAccessType {
+  PRECISE, APPROXIMATE, NONE
+}
 
 /**
  * An implementation of the location service.
@@ -13,6 +22,14 @@ import androidx.activity.result.contract.ActivityResultContracts
  */
 class LocationServiceImpl(private val activity: ComponentActivity) : LocationService {
 
+  /* For the moment, the locationGrantedType exposes the type of location access
+  that the user granted. In the future, it might not be necessary to have it exposed. The
+  granted access will be used internally be the LocationService
+   */
+  private val _locationGrantedType = MutableStateFlow(LocationAccessType.NONE)
+  val locationGrantedType = _locationGrantedType.asStateFlow()
+
+
   // Initialize the ActivityResultLauncher which handles the permission request process
   private val requestPermissionLauncher: ActivityResultLauncher<Array<String>> =
       activity.registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
@@ -20,24 +37,31 @@ class LocationServiceImpl(private val activity: ComponentActivity) : LocationSer
         when {
           // Precise location access granted
           permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
-            Toast.makeText(activity, "Precise location access granted", Toast.LENGTH_LONG).show()
+            _locationGrantedType.value = LocationAccessType.PRECISE
+            Toast.makeText(activity,
+              "Precise location access granted",
+              Toast.LENGTH_LONG).show()
           }
 
           // Only approximate location access granted
           permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
-            Toast.makeText(activity, "Only approximate location access granted", Toast.LENGTH_LONG)
-                .show()
+            _locationGrantedType.value = LocationAccessType.APPROXIMATE
+            Toast.makeText(activity,
+              "Only approximate location access granted",
+              Toast.LENGTH_LONG).show()
           }
 
           // No location access granted
           else -> {
-            Toast.makeText(activity, "No location access granted", Toast.LENGTH_LONG).show()
+            _locationGrantedType.value = LocationAccessType.NONE
+            Toast.makeText(activity,
+              "No location access granted",
+              Toast.LENGTH_LONG).show()
           }
         }
       }
 
   override fun requestUserPermissionForLocation() {
-    // Launch the permission request
     requestPermissionLauncher.launch(
         arrayOf(
             Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION))
