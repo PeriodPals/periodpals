8e864b4f7ea58524335c816ad0315d80c74597d3
agonzalez-r <alejandra.gonzalezrodriguez@epfl.ch>
2024-10-29 07:44:12 +0100

    fix: correct test's errors

--- app/src/androidTest/java/com/android/periodpals/ui/profile/ProfileScreenTest.kt
+++ app/src/androidTest/java/com/android/periodpals/ui/profile/ProfileScreenTest.kt
@@ -5,7 +5,6 @@ import androidx.compose.ui.test.assertIsNotDisplayed
 import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
-import com.android.periodpals.ui.alert.AlertScreen
 import com.android.periodpals.ui.navigation.NavigationActions
 import com.android.periodpals.ui.navigation.Route
 import org.junit.Before
@@ -23,14 +22,13 @@ class ProfileScreenTest {
   fun setUp() {
     navigationActions = mock(NavigationActions::class.java)
 
-    `when`(navigationActions.currentRoute()).thenReturn(Route.ALERT)
+    `when`(navigationActions.currentRoute()).thenReturn(Route.PROFILE)
 
-    composeTestRule.setContent { AlertScreen(navigationActions) }
+    composeTestRule.setContent { ProfileScreen(navigationActions) }
   }
 
   @Test
   fun displayAllComponents() {
-    composeTestRule.setContent { ProfileScreen(navigationActions) }
     composeTestRule.onNodeWithTag("profileAvatar").assertIsDisplayed()
     composeTestRule.onNodeWithTag("profileName").assertIsDisplayed()
     composeTestRule.onNodeWithTag("Description").assertIsDisplayed()
@@ -42,7 +40,6 @@ class ProfileScreenTest {
 
   @Test
   fun profileScreen_hasCorrectContent() {
-    composeTestRule.setContent { ProfileScreen(navigationActions) }
     composeTestRule.onNodeWithTag("profileName").assertTextEquals("Name")
     composeTestRule.onNodeWithTag("Description").assertTextEquals("Description")
   }
