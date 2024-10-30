9a6bd30db67a9137a07c0f2e960453a861aae741
agonzalez-r <alejandra.gonzalezrodriguez@epfl.ch>
2024-10-30 15:43:33 +0100

    test: add test for the navigation of the edit button

--- app/src/androidTest/java/com/android/periodpals/ui/profile/ProfileScreenTest.kt
+++ app/src/androidTest/java/com/android/periodpals/ui/profile/ProfileScreenTest.kt
@@ -1,16 +1,18 @@
 package com.android.periodpals.ui.profile
 
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.assertIsNotDisplayed
 import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
 import com.android.periodpals.ui.navigation.NavigationActions
 import com.android.periodpals.ui.navigation.Route
+import com.android.periodpals.ui.navigation.Screen
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
 import org.mockito.Mockito.`when`
 
 class ProfileScreenTest {
@@ -35,11 +37,17 @@ class ProfileScreenTest {
     composeTestRule.onNodeWithTag("noReviewsCardText").assertIsDisplayed()
     composeTestRule.onNodeWithTag("bottomNavigationMenu").assertIsDisplayed()
     composeTestRule.onNodeWithTag("topBar").assertIsDisplayed()
-    composeTestRule.onNodeWithTag("goBackButton").assertIsNotDisplayed()
+    composeTestRule.onNodeWithTag("editButton").assertIsDisplayed()
   }
 
   @Test
-  fun profileScreen_hasCorrectContent() {
+  fun editButtonNavigatesToEditProfileScreen() {
+    composeTestRule.onNodeWithTag("editButton").performClick()
+    verify(navigationActions).navigateTo(Screen.EDIT_PROFILE)
+  }
+
+  @Test
+  fun profileScreenHasCorrectContent() {
     composeTestRule.onNodeWithTag("profileName").assertTextEquals("Name")
     composeTestRule.onNodeWithTag("Description").assertTextEquals("Description")
   }
