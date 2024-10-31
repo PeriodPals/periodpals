57dfe34eabcc3ea0d4599991e3ccbc1fe4756b57
charlie mangano <eva.mangano@epfl.ch>
2024-10-31 17:14:58 +0100

    refactor: replace hardcoded test tags with `C` values in `SignIn` screen and tests

--- app/src/androidTest/java/com/android/periodpals/ui/authentication/SignInTest.kt
+++ app/src/androidTest/java/com/android/periodpals/ui/authentication/SignInTest.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
 import com.android.periodpals.model.authentication.AuthenticationViewModel
 import com.android.periodpals.model.user.UserAuthState
+import com.android.periodpals.resources.C
 import com.android.periodpals.ui.navigation.NavigationActions
 import com.android.periodpals.ui.navigation.Screen
 import org.junit.Before
@@ -39,82 +40,84 @@ class SignInScreenTest {
 
     `when`(navigationActions.currentRoute()).thenReturn(Screen.SIGN_IN)
     `when`(authViewModel.userAuthState)
-        .thenReturn(mutableStateOf(UserAuthState.Success("User is logged in")))
+      .thenReturn(mutableStateOf(UserAuthState.Success("User is logged in")))
     composeTestRule.setContent { SignInScreen(authViewModel, navigationActions) }
   }
 
   @Test
   fun allComponentsAreDisplayed() {
 
-    composeTestRule.onNodeWithTag("signInScreen").assertIsDisplayed()
-    composeTestRule.onNodeWithTag("signInBackground").assertIsDisplayed()
-    composeTestRule.onNodeWithTag("signInTitle").assertIsDisplayed()
-    composeTestRule.onNodeWithTag("signInInstruction").assertIsDisplayed()
-    composeTestRule.onNodeWithTag("signInEmail").assertIsDisplayed()
-    composeTestRule.onNodeWithTag("signInPassword").assertIsDisplayed()
-    composeTestRule.onNodeWithTag("signInPasswordVisibility").assertIsDisplayed()
-    composeTestRule.onNodeWithTag("signInButton").assertIsDisplayed()
-    composeTestRule.onNodeWithTag("signInOrText").assertIsDisplayed()
-    composeTestRule.onNodeWithTag("signInGoogleButton").assertIsDisplayed()
-    composeTestRule.onNodeWithTag("signInNotRegistered").assertIsDisplayed()
+    composeTestRule.onNodeWithTag(C.Tag.SignInScreen.SCREEN).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(C.Tag.SignInScreen.BACKGROUND).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(C.Tag.SignInScreen.TITLE_TEXT).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(C.Tag.SignInScreen.INSTRUCTION_TEXT).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(C.Tag.SignInScreen.EMAIL_FIELD).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(C.Tag.SignInScreen.PASSWORD_FIELD).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(C.Tag.SignInScreen.PASSWORD_VISIBILITY_BUTTON).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(C.Tag.SignInScreen.SIGN_IN_BUTTON).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(C.Tag.SignInScreen.CONTINUE_WITH_TEXT).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(C.Tag.SignInScreen.GOOGLE_BUTTON).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(C.Tag.SignInScreen.NOT_REGISTERED_BUTTON).assertIsDisplayed()
   }
 
   @Test
   fun emptyEmailShowsCorrectError() {
 
-    composeTestRule.onNodeWithTag("signInPassword").performTextInput(password)
-    composeTestRule.onNodeWithTag("signInButton").performClick()
-    composeTestRule.onNodeWithTag("signInEmailError").assertIsDisplayed()
-    composeTestRule.onNodeWithTag("signInEmailError").assertTextEquals("Email cannot be empty")
+    composeTestRule.onNodeWithTag(C.Tag.SignInScreen.PASSWORD_FIELD).performTextInput(password)
+    composeTestRule.onNodeWithTag(C.Tag.SignInScreen.SIGN_IN_BUTTON).performClick()
+    composeTestRule.onNodeWithTag(C.Tag.SignInScreen.EMAIL_ERROR_TEXT).assertIsDisplayed()
+    composeTestRule
+      .onNodeWithTag(C.Tag.SignInScreen.EMAIL_ERROR_TEXT)
+      .assertTextEquals("Email cannot be empty")
   }
 
   @Test
   fun emptyEmailDoesNotCallVM() {
 
-    composeTestRule.onNodeWithTag("signInPassword").performTextInput(password)
-    composeTestRule.onNodeWithTag("signInButton").performClick()
+    composeTestRule.onNodeWithTag(C.Tag.SignInScreen.PASSWORD_FIELD).performTextInput(password)
+    composeTestRule.onNodeWithTag(C.Tag.SignInScreen.SIGN_IN_BUTTON).performClick()
     verify(authViewModel, never()).logInWithEmail(any(), any())
   }
 
   @Test
   fun emptyPasswordShowsCorrectError() {
 
-    composeTestRule.onNodeWithTag("signInEmail").performTextInput(email)
-    composeTestRule.onNodeWithTag("signInButton").performClick()
-    composeTestRule.onNodeWithTag("signInPasswordError").assertIsDisplayed()
+    composeTestRule.onNodeWithTag(C.Tag.SignInScreen.EMAIL_FIELD).performTextInput(email)
+    composeTestRule.onNodeWithTag(C.Tag.SignInScreen.SIGN_IN_BUTTON).performClick()
+    composeTestRule.onNodeWithTag(C.Tag.SignInScreen.PASSWORD_ERROR_TEXT).assertIsDisplayed()
     composeTestRule
-        .onNodeWithTag("signInPasswordError")
-        .assertTextEquals("Password cannot be empty")
+      .onNodeWithTag(C.Tag.SignInScreen.PASSWORD_ERROR_TEXT)
+      .assertTextEquals("Password cannot be empty")
   }
 
   @Test
   fun emptyPasswordDoesNotCallVM() {
 
-    composeTestRule.onNodeWithTag("signInEmail").performTextInput(email)
-    composeTestRule.onNodeWithTag("signInButton").performClick()
+    composeTestRule.onNodeWithTag(C.Tag.SignInScreen.EMAIL_FIELD).performTextInput(email)
+    composeTestRule.onNodeWithTag(C.Tag.SignInScreen.SIGN_IN_BUTTON).performClick()
     verify(authViewModel, never()).logInWithEmail(any(), any())
   }
 
   @Test
   fun validSignInAttemptNavigatesToProfileScreen() {
 
-    composeTestRule.onNodeWithTag("signInEmail").performTextInput(email)
-    composeTestRule.onNodeWithTag("signInPassword").performTextInput(password)
-    composeTestRule.onNodeWithTag("signInButton").performClick()
+    composeTestRule.onNodeWithTag(C.Tag.SignInScreen.EMAIL_FIELD).performTextInput(email)
+    composeTestRule.onNodeWithTag(C.Tag.SignInScreen.PASSWORD_FIELD).performTextInput(password)
+    composeTestRule.onNodeWithTag(C.Tag.SignInScreen.SIGN_IN_BUTTON).performClick()
     verify(navigationActions).navigateTo(Screen.PROFILE)
   }
 
   @Test
   fun validSignInAttemptCallsVMLogInWithEmail() {
-    composeTestRule.onNodeWithTag("signInEmail").performTextInput(email)
-    composeTestRule.onNodeWithTag("signInPassword").performTextInput(password)
-    composeTestRule.onNodeWithTag("signInButton").performClick()
+    composeTestRule.onNodeWithTag(C.Tag.SignInScreen.EMAIL_FIELD).performTextInput(email)
+    composeTestRule.onNodeWithTag(C.Tag.SignInScreen.PASSWORD_FIELD).performTextInput(password)
+    composeTestRule.onNodeWithTag(C.Tag.SignInScreen.SIGN_IN_BUTTON).performClick()
     verify(authViewModel).logInWithEmail(eq(email), eq(password))
   }
 
   @Test
   fun signUpHereNavigatesToSignUpScreen() {
-    composeTestRule.onNodeWithTag("signInNotRegistered").performClick()
+    composeTestRule.onNodeWithTag(C.Tag.SignInScreen.NOT_REGISTERED_BUTTON).performClick()
     verify(navigationActions).navigateTo(Screen.SIGN_UP)
   }
 }
