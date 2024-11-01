a7bc11c363bf303002aaf4e396580543193c0ae4
francelu <france.lu@epfl.ch>
2024-11-01 15:48:32 +0100

    style: ktfmt format

--- app/src/androidTest/java/com/android/periodpals/ui/map/MapScreenTest.kt
+++ app/src/androidTest/java/com/android/periodpals/ui/map/MapScreenTest.kt
@@ -2,12 +2,10 @@ package com.android.periodpals.ui.map
 
 // UI test for MapViewContainer.kt
 
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.navigation.compose.rememberNavController
 import com.android.periodpals.ui.navigation.NavigationActions
-import com.android.periodpals.ui.theme.PeriodPalsAppTheme
 import org.junit.Rule
 import org.junit.Test
 
@@ -18,9 +16,9 @@ class MapScreenTest {
   @Test
   fun testMapScreenWithPermissionGranted() {
     composeTestRule.setContent {
-        MapScreen(
-            locationPermissionGranted = true,
-            navigationActions = NavigationActions(rememberNavController()))
+      MapScreen(
+          locationPermissionGranted = true,
+          navigationActions = NavigationActions(rememberNavController()))
     }
     // Verify that the map is displayed when permission is granted
     composeTestRule.onNodeWithTag("MapView").assertExists()
@@ -29,9 +27,9 @@ class MapScreenTest {
   @Test
   fun testMapScreenWithoutPermission() {
     composeTestRule.setContent {
-          MapScreen(
-              locationPermissionGranted = false,
-              navigationActions = NavigationActions(rememberNavController()))
+      MapScreen(
+          locationPermissionGranted = false,
+          navigationActions = NavigationActions(rememberNavController()))
     }
     // Verify that the map is still displayed even if permission is not granted
     composeTestRule.onNodeWithTag("MapView").assertExists()
