2816f806bd9c3aefac8eb89b20b98cc137175db2
charlie mangano <eva.mangano@epfl.ch>
2024-11-01 14:58:11 +0100

    docs: add documentation to `C.kt`

--- app/src/main/java/com/android/periodpals/resources/C.kt
+++ app/src/main/java/com/android/periodpals/resources/C.kt
@@ -1,115 +1,130 @@
 package com.android.periodpals.resources
 
+/** The `C` object contains nested objects that define constants used throughout the application. */
 object C {
+
+  /**
+   * The `Tag` object contains nested objects that define constants for tagging UI components in
+   * various screens of the application.
+   */
   object Tag {
+
+    /** Constants for tagging UI components in the AlertScreen. */
     object AlertScreen {
-      const val SCREEN = "screen" // "alertScreen"
-      const val INSTRUCTION_TEXT = "instructionText" // "alertInstruction"
-      const val PRODUCT_FIELD = "productField" // "alertProduct"
-      const val URGENCY_FIELD = "urgencyField" // "alertUrgency"
-      const val LOCATION_FIELD = "locationField" // "alertLocation"
-      const val MESSAGE_FIELD = "messageField" // "alertMessage"
-      const val SUBMIT_BUTTON = "submitButton" // "alertSubmit"
+      const val SCREEN = "screen"
+      const val INSTRUCTION_TEXT = "instructionText"
+      const val PRODUCT_FIELD = "productField"
+      const val URGENCY_FIELD = "urgencyField"
+      const val LOCATION_FIELD = "locationField"
+      const val MESSAGE_FIELD = "messageField"
+      const val SUBMIT_BUTTON = "submitButton"
     }
 
+    /** Constants for tagging UI components in the AlertListScreen. */
     object AlertListScreen {
-      const val SCREEN = "screen" // "alertListScreen"
-      const val TAB_ROW = "tabRow" // "tabRowAlert"
-      const val MY_ALERTS_TAB = "myAlertsTab" // "myAlertsTab"
-      const val PALS_ALERTS_TAB = "palsAlertsTab" // "palsAlertsTab"
-      const val ALERT = "alert" // "alertItem"
-      const val ALERT_ROW = "alertRow" // "alertItemRow"
-      const val ALERT_PROFILE_PICTURE = "profilePicture" // "alertProfilePicture"
-      const val ALERT_NAME = "alertName" // "alertItemName"
-      const val ALERT_TIME = "alertTime" // "alertItemTime"
-      const val ALERT_LOCATION = "alertLocation" // "alertItemLocation"
-      const val ALERT_PRODUCT_TYPE = "alertProductType" // "alertItemProductType"
-      const val ALERT_URGENCY = "alertUrgency" // "alertItemUrgency"
-      const val NO_ALERTS_CARD = "noAlertsCard" // "noAlertsCard"
-      const val NO_ALERTS_ICON = "noAlertsIcon" // "noAlertsIcon"
-      const val NO_ALERTS_TEXT = "noAlertsText" // "noAlertsCardText"
+      const val SCREEN = "screen"
+      const val TAB_ROW = "tabRow"
+      const val MY_ALERTS_TAB = "myAlertsTab"
+      const val PALS_ALERTS_TAB = "palsAlertsTab"
+      const val ALERT = "alert"
+      const val ALERT_ROW = "alertRow"
+      const val ALERT_PROFILE_PICTURE = "profilePicture"
+      const val ALERT_NAME = "alertName"
+      const val ALERT_TIME = "alertTime"
+      const val ALERT_LOCATION = "alertLocation"
+      const val ALERT_PRODUCT_TYPE = "alertProductType"
+      const val ALERT_URGENCY = "alertUrgency"
+      const val NO_ALERTS_CARD = "noAlertsCard"
+      const val NO_ALERTS_ICON = "noAlertsIcon"
+      const val NO_ALERTS_TEXT = "noAlertsText"
     }
 
+    /** Constants for tagging UI components in the SignInScreen. */
     object SignInScreen {
-      const val SCREEN = "screen" // "signInScreen"
-      const val BACKGROUND = "background" // "signInBackground"
-      const val TITLE_TEXT = "titleText" // "signInTitle"
-      const val INSTRUCTION_TEXT = "instructionText" // "signInInstruction"
-      const val EMAIL_FIELD = "emailField" // "signInEmail"
-      const val EMAIL_ERROR_TEXT = "emailErrorText" // "signInEmailError"
-      const val PASSWORD_FIELD = "passwordField" // "signInPassword"
-      const val PASSWORD_VISIBILITY_BUTTON =
-          "passwordVisibilityButton" // "signInPasswordVisibility"
-      const val PASSWORD_ERROR_TEXT = "passwordErrorText" // "signInPasswordError"
-      const val SIGN_IN_BUTTON = "signInButton" // "signInButton"
-      const val CONTINUE_WITH_TEXT = "continueWith" // "signInOrText"
-      const val GOOGLE_BUTTON = "googleButton" // "signInGoogleButton"
-      const val NOT_REGISTERED_BUTTON = "notRegisteredButton" // "signInNotRegistered"
+      const val SCREEN = "screen"
+      const val BACKGROUND = "background"
+      const val TITLE_TEXT = "titleText"
+      const val INSTRUCTION_TEXT = "instructionText"
+      const val EMAIL_FIELD = "emailField"
+      const val EMAIL_ERROR_TEXT = "emailErrorText"
+      const val PASSWORD_FIELD = "passwordField"
+      const val PASSWORD_VISIBILITY_BUTTON = "passwordVisibilityButton"
+      const val PASSWORD_ERROR_TEXT = "passwordErrorText"
+      const val SIGN_IN_BUTTON = "signInButton"
+      const val CONTINUE_WITH_TEXT = "continueWith"
+      const val GOOGLE_BUTTON = "googleButton"
+      const val NOT_REGISTERED_BUTTON = "notRegisteredButton"
     }
 
+    /** Constants for tagging UI components in the SignUpScreen. */
     object SignUpScreen {
-      const val SCREEN = "screen" // "signUpScreen"
-      const val BACKGROUND = "background" // "signUpBackground"
-      const val TITLE_TEXT = "titleText" // "signUpTitle"
-      const val INSTRUCTION_TEXT = "instructionText" // "signUpInstruction"
-      const val EMAIL_FIELD = "emailField" // "signUpEmail"
-      const val EMAIL_ERROR_TEXT = "emailErrorText" // "signUpEmailError"
-      const val PASSWORD_FIELD = "passwordField" // "signUpPassword"
-      const val PASSWORD_VISIBILITY_BUTTON =
-          "passwordVisibilityButton" // "signUpPasswordVisibility"
-      const val PASSWORD_ERROR_TEXT = "passwordErrorText" // "signUpPasswordError"
-      const val CONFIRM_PASSWORD_TEXT = "confirmText" // "signUpConfirmText"
-      const val CONFIRM_PASSWORD_FIELD = "confirmPasswordField" // "signUpConfirmPassword"
-      const val CONFIRM_PASSWORD_VISIBILITY_BUTTON =
-          "confirmPasswordVisibilityButton" // "signUpConfirmVisibility"
-      const val CONFIRM_PASSWORD_ERROR_TEXT = "confirmPasswordErrorText" // "signUpConfirmError"
-      const val SIGN_UP_BUTTON = "signUpButton" // "signUpButton"
+      const val SCREEN = "screen"
+      const val BACKGROUND = "background"
+      const val TITLE_TEXT = "titleText"
+      const val INSTRUCTION_TEXT = "instructionText"
+      const val EMAIL_FIELD = "emailField"
+      const val EMAIL_ERROR_TEXT = "emailErrorText"
+      const val PASSWORD_FIELD = "passwordField"
+      const val PASSWORD_VISIBILITY_BUTTON = "passwordVisibilityButton"
+      const val PASSWORD_ERROR_TEXT = "passwordErrorText"
+      const val CONFIRM_PASSWORD_TEXT = "confirmText"
+      const val CONFIRM_PASSWORD_FIELD = "confirmPasswordField"
+      const val CONFIRM_PASSWORD_VISIBILITY_BUTTON = "confirmPasswordVisibilityButton"
+      const val CONFIRM_PASSWORD_ERROR_TEXT = "confirmPasswordErrorText"
+      const val SIGN_UP_BUTTON = "signUpButton"
     }
 
+    /** Constants for tagging UI components in the MapScreen. */
     object MapScreen {
-      const val SCREEN = "screen" // "MapScreen"
+      const val SCREEN = "screen"
     }
 
+    /** Constants for tagging UI components in the BottomNavigationMenu. */
     object BottomNavigationMenu {
-      const val BOTTOM_NAVIGATION_MENU = "bottomNavigationMenu" // "bottomNavigationMenu"
-      const val BOTTOM_NAVIGATION_MENU_ITEM = "bottomNavigationMenu" // "navigationBarItem"
+      const val BOTTOM_NAVIGATION_MENU = "bottomNavigationMenu"
+      const val BOTTOM_NAVIGATION_MENU_ITEM = "bottomNavigationMenu"
     }
 
+    /** Constants for tagging UI components in the TopAppBar. */
     object TopAppBar {
-      const val TOP_BAR = "topBar" // "topBar"
-      const val GO_BACK_BUTTON = "goBackButton" // "goBackButton"
-      const val EDIT_BUTTON = "editButton" // "editButton"
-      const val TITLE_TEXT = "titleText" // "screenTitle"
+      const val TOP_BAR = "topBar"
+      const val GO_BACK_BUTTON = "goBackButton"
+      const val EDIT_BUTTON = "editButton"
+      const val TITLE_TEXT = "titleText"
     }
 
+    /** Constants for tagging UI components in the CreateProfileScreen. */
     object CreateProfileScreen {
-      const val SCREEN = "screen" // "createProfileScreen"
-      const val PROFILE_PICTURE = "profilePicture" // "profile_image"
-      const val EMAIL_FIELD = "emailField" // "email_field"
-      const val DOB_FIELD = "dobField" // "dob_field"
-      const val NAME_FIELD = "nameField" // "name_field"
-      const val DESCRIPTION_FIELD = "descriptionField" // "description_field"
-      const val SAVE_BUTTON = "saveButton" // "save_button"
+      const val SCREEN = "screen"
+      const val PROFILE_PICTURE = "profilePicture"
+      const val EMAIL_FIELD = "emailField"
+      const val DOB_FIELD = "dobField"
+      const val NAME_FIELD = "nameField"
+      const val DESCRIPTION_FIELD = "descriptionField"
+      const val SAVE_BUTTON = "saveButton"
     }
 
+    /** Constants for tagging UI components in the EditProfileScreen. */
     object EditProfileScreen {
-      const val SCREEN = "screen" // "editProfileScreen"
+      const val SCREEN = "screen"
     }
 
+    /** Constants for tagging UI components in the ProfileScreen. */
     object ProfileScreen {
-      const val SCREEN = "screen" // "profileScreen"
-      const val PROFILE_PICTURE = "profilePicture" // "profileAvatar"
-      const val NAME_FIELD = "nameField" // "profileName"
-      const val DESCRIPTION_FIELD = "descriptionField" // "description"
-      const val REVIEW_ONE = "reviewOne" // "reviewOne"
-      const val REVIEW_TWO = "reviewTwo" // "reviewTwo"
-      const val NO_REVIEWS_TEXT = "noReviewsText" // "noReviewsCardText"
+      const val SCREEN = "screen"
+      const val PROFILE_PICTURE = "profilePicture"
+      const val NAME_FIELD = "nameField"
+      const val DESCRIPTION_FIELD = "descriptionField"
+      const val REVIEW_ONE = "reviewOne"
+      const val REVIEW_TWO = "reviewTwo"
+      const val NO_REVIEWS_TEXT = "noReviewsText"
       const val NO_REVIEWS_CARD = "noReviewsCard"
     }
 
+    /** Constants for tagging UI components in the TimerScreen. */
     object TimerScreen {
-      const val SCREEN = "screen" // "timerScreen"
-      const val TIMER_TEXT = "timerText" // "timerText"
+      const val SCREEN = "screen"
+      const val TIMER_TEXT = "timerText"
     }
   }
 }
