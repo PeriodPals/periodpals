7eb3bdcf76c0c2e656f0d549db7965f32d8f051f
francelu <france.lu@epfl.ch>
2024-11-03 10:42:08 +0100

    test: add tests to verify all components are displayed in MapScreenTest

--- app/src/androidTest/java/com/android/periodpals/ui/map/MapScreenTest.kt
+++ app/src/androidTest/java/com/android/periodpals/ui/map/MapScreenTest.kt
@@ -2,25 +2,51 @@ package com.android.periodpals.ui.map
 
 // UI test for MapViewContainer.kt
 
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsNotDisplayed
+import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
-import androidx.navigation.compose.rememberNavController
+import com.android.periodpals.resources.C.Tag.MapScreen
+import com.android.periodpals.resources.C.Tag.TopAppBar
 import com.android.periodpals.ui.navigation.NavigationActions
+import com.android.periodpals.ui.navigation.Route
 import com.android.periodpals.ui.theme.PeriodPalsAppTheme
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when`
 
 class MapScreenTest {
 
+  private lateinit var navigationActions: NavigationActions
   @get:Rule val composeTestRule = createComposeRule()
+
+  @Before
+  fun setUp() {
+    navigationActions = mock(NavigationActions::class.java)
+
+    `when`(navigationActions.currentRoute()).thenReturn(Route.ALERT)
+  }
+
+  @Test
+  fun allComponentsAreDisplayed() {
+    composeTestRule.setContent {
+      MapScreen(locationPermissionGranted = true, navigationActions = navigationActions)
+    }
+    composeTestRule.onNodeWithTag(MapScreen.SCREEN).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(TopAppBar.TOP_BAR).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(TopAppBar.TITLE_TEXT).assertIsDisplayed().assertTextEquals("Map")
+    composeTestRule.onNodeWithTag(TopAppBar.GO_BACK_BUTTON).assertIsNotDisplayed()
+    composeTestRule.onNodeWithTag(TopAppBar.EDIT_BUTTON).assertIsNotDisplayed()
+  }
 
   @Test
   fun testMapScreenWithPermissionGranted() {
     composeTestRule.setContent {
       PeriodPalsAppTheme {
-        MapScreen(
-            locationPermissionGranted = true,
-            navigationActions = NavigationActions(rememberNavController()))
+        MapScreen(locationPermissionGranted = true, navigationActions = navigationActions)
       }
     }
     // Verify that the map is displayed when permission is granted
@@ -31,9 +57,7 @@ class MapScreenTest {
   fun testMapScreenWithoutPermission() {
     composeTestRule.setContent {
       PeriodPalsAppTheme {
-        MapScreen(
-            locationPermissionGranted = false,
-            navigationActions = NavigationActions(rememberNavController()))
+        MapScreen(locationPermissionGranted = false, navigationActions = navigationActions)
       }
     }
     // Verify that the map is still displayed even if permission is not granted
