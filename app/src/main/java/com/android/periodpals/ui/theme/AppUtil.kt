69c98788660c65626868cdc30ea26061e63a098c
francelu <france.lu@epfl.ch>
2024-11-03 09:41:36 +0100

    docs: add documentation to AppUtil

--- app/src/main/java/com/android/periodpals/ui/theme/AppUtil.kt
+++ app/src/main/java/com/android/periodpals/ui/theme/AppUtil.kt
@@ -5,6 +5,12 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.runtime.remember
 
+/**
+ * A composable function that provides the application dimensions to the composition.
+ *
+ * @param appDimens The dimensions to be provided.
+ * @param content The content composable that will have access to the provided dimensions.
+ */
 @Composable
 fun ProvideAppUtils(
     appDimens: Dimens,
