af3dca431e0d589de30c3d4bb2c0445551f85a31
agonzalez-r <alejandra.gonzalezrodriguez@epfl.ch>
2024-11-04 17:36:31 +0100

    style: ktfmt format

--- app/src/main/java/com/android/periodpals/model/alert/Alert.kt
+++ app/src/main/java/com/android/periodpals/model/alert/Alert.kt
@@ -1,54 +1,36 @@
 package com.android.periodpals.model.alert
 
+import java.time.LocalDateTime
 import kotlinx.serialization.Contextual
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import java.time.LocalDateTime
 
 @Serializable
 data class Alert(
-    @SerialName("idAlert")
-    val idAlert: String,
-
-    @SerialName("idProfile")
-    val idProfile: String,
-
-    @SerialName("name")
-    val name: String,
-
-    @SerialName("product")
-    val product: Product,
-
-    @SerialName("urgency")
-    val urgency: Urgency,
-
-    @SerialName("createdAt")
-    @Contextual
-    val createdAt: LocalDateTime,
-
-    @SerialName("location")
-    val location: String,       //TODO: Create data class Location
-
-    @SerialName("message")
-    val message: String,
-
-    @SerialName("status")
-    val status: Status
+    @SerialName("idAlert") val idAlert: String,
+    @SerialName("idProfile") val idProfile: String,
+    @SerialName("name") val name: String,
+    @SerialName("product") val product: Product,
+    @SerialName("urgency") val urgency: Urgency,
+    @SerialName("createdAt") @Contextual val createdAt: LocalDateTime,
+    @SerialName("location") val location: String, // TODO: Create data class Location
+    @SerialName("message") val message: String,
+    @SerialName("status") val status: Status
 )
 
 enum class Product {
-    TAMPON,
-    PAD
+  TAMPON,
+  PAD
 }
 
 enum class Urgency {
-    URGENT,
-    NORMAL,
-    NOT_URGENT,
+  URGENT,
+  NORMAL,
+  NOT_URGENT,
 }
 
 enum class Status {
-    CREATED,        // The alert has been created and is waiting for someone to respond
-    PENDING,        // Someone has acknowledged the alert and is helping
-    SOLVED,         // The alert has been resolved, help was provided
+  CREATED, // The alert has been created and is waiting for someone to respond
+  PENDING, // Someone has acknowledged the alert and is helping
+  SOLVED, // The alert has been resolved, help was provided
 }
