9062874f6947ca2fe3e55ec41cbdf9ddb4959934
charlie mangano <eva.mangano@epfl.ch>
2024-11-04 17:28:05 +0100

    merge: continue merging branch `origin/main` into `feat/alert/cleanup` (`AlertListsScreenTest.kt`)

--- app/src/androidTest/java/com/android/periodpals/ui/alert/AlertListsScreenTest.kt
+++ app/src/androidTest/java/com/android/periodpals/ui/alert/AlertListsScreenTest.kt
@@ -1,6 +1,7 @@
 package com.android.periodpals.ui.alert
 
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsNotDisplayed
 import androidx.compose.ui.test.assertIsNotSelected
 import androidx.compose.ui.test.assertIsSelected
 import androidx.compose.ui.test.assertTextEquals
@@ -9,6 +10,7 @@ import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.android.periodpals.resources.C.Tag.AlertListsScreen
+import com.android.periodpals.resources.C.Tag.TopAppBar
 import com.android.periodpals.ui.navigation.NavigationActions
 import com.android.periodpals.ui.navigation.Route
 import org.junit.Before
@@ -39,10 +41,17 @@ class AlertListsScreenTest {
   fun sharedComponentsCorrectlyDisplayed() {
     composeTestRule.setContent { AlertListsScreen(navigationActions) }
 
-    composeTestRule.onNodeWithTag(AlertListsScreen.SCREEN).assertExists()
+    composeTestRule.onNodeWithTag(AlertListsScreen.SCREEN).assertIsDisplayed()
     composeTestRule.onNodeWithTag(AlertListsScreen.TAB_ROW).assertIsDisplayed()
     composeTestRule.onNodeWithTag(AlertListsScreen.MY_ALERTS_TAB).assertIsDisplayed()
     composeTestRule.onNodeWithTag(AlertListsScreen.PALS_ALERTS_TAB).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(TopAppBar.TOP_BAR).assertIsDisplayed()
+    composeTestRule
+      .onNodeWithTag(TopAppBar.TITLE_TEXT)
+      .assertIsDisplayed()
+      .assertTextEquals("Alert Lists")
+    composeTestRule.onNodeWithTag(TopAppBar.GO_BACK_BUTTON).assertIsNotDisplayed()
+    composeTestRule.onNodeWithTag(TopAppBar.EDIT_BUTTON).assertIsNotDisplayed()
   }
 
   @Test
