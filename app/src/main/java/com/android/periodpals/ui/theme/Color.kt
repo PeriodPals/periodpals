4e5b73c162fc7f17fb33895c3cda9b0a9fd6526b
francelu <france.lu@epfl.ch>
2024-11-05 21:54:31 +0100

    style: ktfmt

--- app/src/main/java/com/android/periodpals/ui/theme/Color.kt
+++ app/src/main/java/com/android/periodpals/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.android.periodpals.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-//TODO: delete
+// TODO: delete
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
