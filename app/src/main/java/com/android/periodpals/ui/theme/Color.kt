2975a3566dc53b46dcba0b810d47d2652aad5570
francelu <france.lu@epfl.ch>
2024-11-05 18:55:27 +0100

    fix: typo in `Color.kt`

--- app/src/main/java/com/android/periodpals/ui/theme/Color.kt
+++ app/src/main/java/com/android/periodpals/ui/theme/Color.kt
@@ -2,6 +2,7 @@ package com.android.periodpals.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+//TODO: delete
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
@@ -10,7 +11,7 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-/* Light theme, still in development and thus not in use */
+/* Light theme */
 val primaryLight = Color(0xFF415F91)
 val onPrimaryLight = Color(0xFFFFFFFF)
 val primaryContainerLight = Color(0xFFD6E3FF)
@@ -47,7 +48,7 @@ val surfaceContainerLight = Color(0xFFEDEDF4)
 val surfaceContainerHighLight = Color(0xFFE7E8EE)
 val surfaceContainerHighestLight = Color(0xFFE2E2E9)
 
-/* Dark theme, still in development and thus not in use */
+/* Dark theme */
 val primaryDark = Color(0xFFAAC7FF)
 val onPrimaryDark = Color(0xFF0A305F)
 val primaryContainerDark = Color(0xFF284777)
