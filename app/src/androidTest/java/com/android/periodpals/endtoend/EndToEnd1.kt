77b764eeefa404ecd24d3316f8265e28fcbe592c
charlie mangano <eva.mangano@epfl.ch>
2024-11-07 15:28:30 +0100

    fix: make end to end tests work
    Removed useless `screens. <...>ScreenScreen` classes and related code in `EndToEnd1.kt`
    
    Added an `ActivityTestRule` for `MainActivity` in the tests to be able to set the main activity as content of the `composeTestRule`.
    
    Made the signup email random.
    
    Changed testTags to use those contained in `C.Tag`

--- app/src/androidTest/java/com/android/periodpals/endtoend/EndToEnd1.kt
+++ app/src/androidTest/java/com/android/periodpals/endtoend/EndToEnd1.kt
@@ -1,40 +1,41 @@
 package com.android.periodpals.endtoend
 
+import android.util.Log
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
 import com.android.periodpals.MainActivity
-import com.android.periodpals.screens.CreateProfileScreenScreen
-import com.android.periodpals.screens.ProfileScreenScreen
-import com.android.periodpals.screens.SignInScreenScreen
-import com.android.periodpals.screens.SignUpScreenScreen
-import io.github.kakaocup.compose.node.element.ComposeScreen
+import com.android.periodpals.resources.C.Tag
+import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.UUID
+
+private const val TAG = "EndToEnd1"
 
 /**
  * This end-to-end test checks the user flow for signing up a new user. See "SignUp" user flow (M1:
  * User Flow 1)
  */
 @RunWith(AndroidJUnit4::class)
-class EndToEnd1 {
-  @get:Rule val composeTestRule = createAndroidComposeRule<MainActivity>()
+class EndToEnd1 : TestCase() {
+  @get:Rule val composeTestRule = createComposeRule()
+  @get:Rule val activityRule = ActivityTestRule(MainActivity::class.java)
 
   companion object {
-    private const val email = "ada_lovelace@epfl.ch"
-    private const val psswd = "iLoveSwent1234!"
+    private val email = UUID.randomUUID().toString() + "@example.com"
+    private val psswd = "iLoveSwent1234!"
 
     private const val name = "Ada Lovelace"
     private const val dob = "10/12/1852"
     private const val description = "I'm an English mathematician"
-
-    // Define user view model
   }
 
   @Before fun setUp() {}
@@ -47,34 +48,64 @@ class EndToEnd1 {
   @Test
   fun signUpEndToEnd() {
 
+    composeTestRule.setContent { MainActivity() }
+
     // User arrives on SignIn Screen and navs towards SignUp Screen
-    ComposeScreen.onComposeScreen<SignInScreenScreen>(composeTestRule) {
-      composeTestRule.onNodeWithTag("signInNotRegistered").assertIsDisplayed().performClick()
-    }
+    composeTestRule.waitForIdle()
+    Log.d(TAG, "User arrives on SignIn Screen")
+    composeTestRule.onNodeWithTag(Tag.SignInScreen.SCREEN).assertExists()
+    composeTestRule
+      .onNodeWithTag(Tag.SignInScreen.NOT_REGISTERED_BUTTON)
+      .assertIsDisplayed()
+      .performClick()
 
     // User Signs Up and proceeds to create profile
-    ComposeScreen.onComposeScreen<SignUpScreenScreen>(composeTestRule) {
-      composeTestRule.onNodeWithTag("signUpEmail").assertIsDisplayed().performTextInput(email)
-      composeTestRule.onNodeWithTag("signUpPassword").assertIsDisplayed().performTextInput(psswd)
-      composeTestRule.onNodeWithTag("signUpConfirmText").assertIsDisplayed().performTextInput(psswd)
-      composeTestRule.onNodeWithTag("signUpButton").assertIsDisplayed().performClick()
-    }
+    composeTestRule.waitForIdle()
+    Log.d(TAG, "User arrives on SignUp Screen")
+    composeTestRule
+      .onNodeWithTag(Tag.SignUpScreen.EMAIL_FIELD)
+      .assertIsDisplayed()
+      .performTextInput(email)
+    composeTestRule
+      .onNodeWithTag(Tag.SignUpScreen.PASSWORD_FIELD)
+      .assertIsDisplayed()
+      .performTextInput(psswd)
+    composeTestRule
+      .onNodeWithTag(Tag.SignUpScreen.CONFIRM_PASSWORD_FIELD)
+      .assertIsDisplayed()
+      .performTextInput(psswd)
+    composeTestRule
+      .onNodeWithTag(Tag.SignUpScreen.SIGN_UP_BUTTON)
+      .assertIsDisplayed()
+      .performClick()
 
     // Fill up profile
-    ComposeScreen.onComposeScreen<CreateProfileScreenScreen>(composeTestRule) {
-      composeTestRule.onNodeWithTag("email_field").assertIsDisplayed().performTextInput(email)
-      composeTestRule.onNodeWithTag("name_field").assertIsDisplayed().performTextInput(name)
-      composeTestRule.onNodeWithTag("dob_field").assertIsDisplayed().performTextInput(dob)
-      composeTestRule
-          .onNodeWithTag("description_field")
-          .assertIsDisplayed()
-          .performTextInput(description)
-      composeTestRule.onNodeWithTag("save_button").assertIsDisplayed().performClick()
-    }
+    composeTestRule.waitForIdle()
+    Log.d(TAG, "User arrives on Create Profile Screen")
+    composeTestRule
+      .onNodeWithTag(Tag.CreateProfileScreen.EMAIL_FIELD)
+      .assertIsDisplayed()
+      .performTextInput(email)
+    composeTestRule
+      .onNodeWithTag(Tag.CreateProfileScreen.NAME_FIELD)
+      .assertIsDisplayed()
+      .performTextInput(name)
+    composeTestRule
+      .onNodeWithTag(Tag.CreateProfileScreen.DOB_FIELD)
+      .assertIsDisplayed()
+      .performTextInput(dob)
+    composeTestRule
+      .onNodeWithTag(Tag.CreateProfileScreen.DESCRIPTION_FIELD)
+      .assertIsDisplayed()
+      .performTextInput(description)
+    composeTestRule
+      .onNodeWithTag(Tag.CreateProfileScreen.SAVE_BUTTON)
+      .assertIsDisplayed()
+      .performClick()
 
     // Profile Screen
-    ComposeScreen.onComposeScreen<ProfileScreenScreen>(composeTestRule) {
-      composeTestRule.onNodeWithTag("profileScreen").assertExists()
-    }
+    composeTestRule.waitForIdle()
+    Log.d(TAG, "User arrives on Profile Screen")
+    composeTestRule.onNodeWithTag(Tag.ProfileScreen.SCREEN).assertExists()
   }
 }
