d71205e05183f4b262515ce60cb300be7f5b5c36
francelu <france.lu@epfl.ch>
2024-11-10 15:22:52 +0100

    fix: remove `onClick` for `ProfilePicture` of `EditProfileScreen`
    - Edit profile picture is done through the edit icon.

--- app/src/main/java/com/android/periodpals/ui/profile/EditProfile.kt
+++ app/src/main/java/com/android/periodpals/ui/profile/EditProfile.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.material3.Button
@@ -32,8 +31,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
@@ -58,7 +55,6 @@ import com.android.periodpals.ui.navigation.NavigationActions
 import com.android.periodpals.ui.navigation.Screen
 import com.android.periodpals.ui.navigation.TopAppBar
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
-import com.bumptech.glide.integration.compose.GlideImage
 
 private const val SCREEN_TITLE = "Edit Your Profile"
 
@@ -108,10 +104,6 @@ fun EditProfileScreen(navigationActions: NavigationActions) {
           Box(modifier = Modifier.size(190.dp)) {
             ProfilePicture(
                 profileImageUri,
-                onClick = {
-                  val pickImageIntent = Intent(Intent.ACTION_PICK).apply { type = "image/*" }
-                  launcher.launch(pickImageIntent)
-                },
                 testTag = EditProfileScreen.PROFILE_PICTURE,
             )
 
