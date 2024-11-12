119dc420a8050900cc7277b769c7344c40c9f0a6
taghizadlaura <laura.taghizad@epfl.ch>
2024-11-12 09:33:47 +0100

    fix: add a Log.d to the onFailure in LocationViewModel.

--- app/src/main/java/com/android/periodpals/model/location/LocationViewModel.kt
+++ app/src/main/java/com/android/periodpals/model/location/LocationViewModel.kt
@@ -1,5 +1,6 @@
 package com.android.periodpals.model.location
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -28,7 +29,10 @@ class LocationViewModel(val repository: LocationModel) : ViewModel() {
     query_.value = query
 
     if (query.isNotEmpty()) {
-      repository.search(query, { locationSuggestions_.value = it }, {})
+      repository.search(
+          query,
+          { locationSuggestions_.value = it },
+          { Log.d("SearchError", "Failed to fetch location suggestions for query: $query") })
     }
   }
 }
