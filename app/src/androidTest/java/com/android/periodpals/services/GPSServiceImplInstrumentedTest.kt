24a0e2b3be5d6344d1294ade5afad29baeb56531
Bruno Lazarini Sigg <bruno.lazarinisigg@epfl.ch>
2024-11-12 19:35:28 +0100

    Test: Implemented instrumented tests to verify correct behaviour of GPSServiceImpl

---
+++ app/src/androidTest/java/com/android/periodpals/services/GPSServiceImplInstrumentedTest.kt
@@ -0,0 +1,117 @@
+package com.android.periodpals.services
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.GrantPermissionRule
+import android.Manifest
+import androidx.activity.ComponentActivity
+import androidx.lifecycle.Lifecycle
+import androidx.test.core.app.ActivityScenario
+import com.android.periodpals.model.location.GPSLocation
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Assert.assertNotEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class GPSServiceImplInstrumentedTest {
+
+  @get:Rule
+  val permissionRule: GrantPermissionRule = GrantPermissionRule.grant(
+    Manifest.permission.ACCESS_FINE_LOCATION,
+    Manifest.permission.ACCESS_COARSE_LOCATION
+  )
+
+  private lateinit var scenario: ActivityScenario<ComponentActivity>
+  private lateinit var activity: ComponentActivity
+  private lateinit var gpsService: GPSServiceImpl
+
+  // Default location
+  private val defaultLat = GPSLocation.DEFAULT_LOCATION.lat
+  private val defaultLong = GPSLocation.DEFAULT_LOCATION.long
+
+  @Before
+  fun setup() {
+
+    scenario = ActivityScenario.launch(ComponentActivity::class.java)
+
+    scenario.moveToState(Lifecycle.State.CREATED)
+
+    scenario.onActivity { activity ->
+      this.activity = activity
+      gpsService = GPSServiceImpl(this.activity)
+    }
+
+    scenario.moveToState(Lifecycle.State.RESUMED)
+  }
+
+  @After
+  fun tearDownService() {
+    gpsService.cleanup()
+  }
+
+  @Test
+  fun testShouldReceiveLocationUpdate() = runTest {
+    // Start location updates
+    gpsService.askPermissionAndStartUpdates()
+
+    // Wait for the update to happen
+    val updatedLocation = gpsService.location.first { location ->
+      location.lat != defaultLat || location.long != defaultLong
+    }
+
+    // The received location should be different than the default
+    assertNotEquals(defaultLat, updatedLocation.lat)
+    assertNotEquals(defaultLong, updatedLocation.long)
+  }
+
+  @Test
+  fun testSwitchingLocationAccuracy() = runTest {
+    // Start with precise location
+    gpsService.askPermissionAndStartUpdates()
+
+    // Switch to approximate
+    gpsService.switchFromPreciseToApproximate()
+
+    // Wait for the update to happen and capture approx location
+    val approxLocation = gpsService.location.first { location ->
+      location.lat != defaultLat || location.long != defaultLong
+    }
+
+    // Switch back to precise
+    gpsService.switchFromApproximateToPrecise()
+
+    // Wait for update to happen and capture precise location
+    val preciseLocation = gpsService.location.first { location ->
+      location.lat != approxLocation.lat || location.long != approxLocation.long
+    }
+
+    // Locations captured should be different
+    assertNotEquals(approxLocation.lat, preciseLocation.lat)
+    assertNotEquals(approxLocation.long, preciseLocation.long)
+  }
+
+  @Test
+  fun testCleanupStopsUpdates() = runTest {
+    // Start updates
+    gpsService.askPermissionAndStartUpdates()
+
+    // Get updated location
+    val updatedLocation = gpsService.location.first { location ->
+      location.lat != defaultLat || location.long != defaultLong
+    }
+
+    // Stop updates
+    gpsService.cleanup()
+
+    // Verify that the location has not changed
+    val finalLocation = gpsService.location.first()
+
+    // Location shouldn't have changed
+    assert(updatedLocation.lat == finalLocation.lat )
+    assert(updatedLocation.long == finalLocation.long)
+  }
+}
