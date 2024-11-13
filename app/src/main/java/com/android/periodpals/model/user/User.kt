a4ed62ae3a6bfc46961914665cc6037054dec345
Alonso <alonso.coaguila@epfl.ch>
2024-11-13 01:35:12 +0100

    feat: add `asUserDto` function to `User` for ease of translation

--- app/src/main/java/com/android/periodpals/model/user/User.kt
+++ app/src/main/java/com/android/periodpals/model/user/User.kt
@@ -8,4 +8,9 @@ package com.android.periodpals.model.user
  * @property description A brief description of the user.
  * @property dob The date of birth of the user.
  */
-data class User(val name: String, val imageUrl: String, val description: String, val dob: String)
+data class User(val name: String, val imageUrl: String, val description: String, val dob: String){
+    inline fun asUserDto(): UserDto {
+        return UserDto(
+            name = this.name, imageUrl = this.imageUrl, description = this.description, dob = this.dob)
+    }
+}
