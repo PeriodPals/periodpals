ca9367ae2322e42e021422764a03e0db320bd8cf
Bruno Lazarini Sigg <bruno.lazarinisigg@epfl.ch>
2024-11-14 16:04:59 +0100

    Style: ktfmt

--- app/src/main/java/com/android/periodpals/services/GPSService.kt
+++ app/src/main/java/com/android/periodpals/services/GPSService.kt
@@ -3,45 +3,40 @@ package com.android.periodpals.services
 /**
  * Interface to interact with the GPS in the device.
  *
- * Defines functions to ask for location access and to fetch the geographical
- * coordinates of the device.
- *
+ * Defines functions to ask for location access and to fetch the geographical coordinates of the
+ * device.
  */
 interface GPSService {
 
   /**
    * Verifies whether access to the location of the device has already been granted.
    * - If granted, it starts the fetching the device location.
-   * - Otherwise, it first launches the system permission dialog then starts the updates if permission is granted.
-   * Moreover, it also verifies that the location is not being already tracked.
+   * - Otherwise, it first launches the system permission dialog then starts the updates if
+   *   permission is granted. Moreover, it also verifies that the location is not being already
+   *   tracked.
    */
   fun askPermissionAndStartUpdates()
 
   /**
    * Switches from precise to approximate location updates.
-   *
    * - **Precise** updates demand more power.
    * - **Approximate** updates demand less power.
    *
-   * Before switching, it verifies that permission is granted, the app is already fetching the device
-   * location and that the current mode up is precise.
+   * Calling this function will have no effect if permissions are not granted or if no location
+   * updates are being received.
    */
   fun switchFromPreciseToApproximate()
 
   /**
    * Switches from approximate to precise location updates.
-   *
    * - **Precise** updates demand more power.
    * - **Approximate** updates demand less power.
    *
-   * Before switching, it verifies that permission is granted, the app is already fetching the device
-   * location and that the current mode up is approximate.
+   * Calling this function will have no effect if permissions are not granted or if no location
+   * updates are being received.
    */
   fun switchFromApproximateToPrecise()
 
-
-  /**
-   * Stops fetching the user location and cleans up the resources.
-   */
+  /** Stops fetching the user location and cleans up the resources. */
   fun cleanup()
 }
