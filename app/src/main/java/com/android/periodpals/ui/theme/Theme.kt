88842b7811d2d756b93a6e7116d98fa0169e571e
francelu <france.lu@epfl.ch>
2024-11-14 12:18:23 +0100

    fix: remove Toast for screen size

--- app/src/main/java/com/android/periodpals/ui/theme/Theme.kt
+++ app/src/main/java/com/android/periodpals/ui/theme/Theme.kt
@@ -1,7 +1,6 @@
 package com.android.periodpals.ui.theme
 
 import android.app.Activity
-import android.widget.Toast
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
@@ -46,41 +45,33 @@ fun PeriodPalsAppTheme(
 
   var appDimens = CompactLargeDimens
   var typography = CompactLargeTypography
-  var toasty = ""
   when (window.widthSizeClass) {
     WindowWidthSizeClass.Compact -> {
       if (config.screenWidthDp <= COMPACT_S) {
         // Emulator Small Phone API 34
         appDimens = CompactSmallDimens
         typography = CompactSmallTypography
-        toasty = "CompactSmall"
       } else if (config.screenWidthDp <= COMPACT_M) {
         // Emulator Pixel 2 API 34
         appDimens = CompactMediumDimens
         typography = CompactMediumTypography
-        toasty = "CompactMedium"
       } else {
         // Emulator Pixel 9 Pro XL API 34
         appDimens = CompactLargeDimens
         typography = CompactLargeTypography
-        toasty = "CompactLarge"
       }
     }
     WindowWidthSizeClass.Medium -> {
       // Emulator Medium Tablet API 34
       appDimens = MediumDimens
       typography = MediumTypography
-      toasty = "Medium"
     }
     WindowWidthSizeClass.Expanded -> {
       // Pixel C API 34
       appDimens = ExpandedDimens
       typography = ExpandedTypography
-      toasty = "Expanded"
     }
   }
-  // TODO: delete
-  Toast.makeText(activity, toasty, Toast.LENGTH_SHORT).show()
 
   ProvideAppUtils(appDimens = appDimens) {
     MaterialTheme(colorScheme = colorScheme, typography = typography, content = content)
