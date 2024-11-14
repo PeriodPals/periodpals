475249858a798708361f0459b9a0a817bc160f51
Bruno Lazarini Sigg <bruno.lazarinisigg@epfl.ch>
2024-11-14 17:17:42 +0100

    fix: update test tag in MapScreenTest.kt

--- app/src/androidTest/java/com/android/periodpals/ui/map/MapScreenTest.kt
+++ app/src/androidTest/java/com/android/periodpals/ui/map/MapScreenTest.kt
@@ -6,6 +6,7 @@ import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import com.android.periodpals.model.location.GPSLocation
+import com.android.periodpals.resources.C
 import com.android.periodpals.resources.C.Tag.MapScreen
 import com.android.periodpals.resources.C.Tag.TopAppBar
 import com.android.periodpals.services.GPSServiceImpl
@@ -47,7 +48,6 @@ class MapScreenTest {
     composeTestRule.onNodeWithTag(TopAppBar.GO_BACK_BUTTON).assertIsNotDisplayed()
     composeTestRule.onNodeWithTag(TopAppBar.EDIT_BUTTON).assertIsNotDisplayed()
 
-    // TODO: Change logic here with C.kt file test tags
-    composeTestRule.onNodeWithTag("MapView").assertIsDisplayed()
+    composeTestRule.onNodeWithTag(MapScreen.MAP_VIEW_CONTAINER).assertIsDisplayed()
   }
 }
