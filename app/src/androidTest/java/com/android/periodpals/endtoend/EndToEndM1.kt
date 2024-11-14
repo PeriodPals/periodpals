dfbcff4255ab4a87b621820890c76f6a8775e3a1
charlie mangano <eva.mangano@epfl.ch>
2024-11-14 22:13:44 +0100

    feat: implement end-to-end tests for SignIn flow

--- app/src/androidTest/java/com/android/periodpals/endtoend/EndToEndM1.kt
+++ app/src/androidTest/java/com/android/periodpals/endtoend/EndToEndM1.kt
@@ -25,23 +25,22 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-private const val TAG = "EndToEnd1"
+private const val TAG = "EndToEndM1"
 
-/**
- * This end-to-end test checks the user flow for signing up a new user. See "SignUp" user flow (M1:
- * User Flow 1)
- */
 @RunWith(AndroidJUnit4::class)
 class EndToEnd1 : TestCase() {
   @get:Rule val composeTestRule = createComposeRule()
   @get:Rule val activityRule = ActivityTestRule(MainActivity::class.java)
 
-  companion object {
-    private val name = ('a'..'z').map { it }.shuffled().subList(0, 8).joinToString("")
+  companion object SignUpData {
+    private val signUpName = ('a'..'z').map { it }.shuffled().subList(0, 8).joinToString("")
     private const val dob = "01/01/2001"
-    private val description = "Short bio containing my name: ${name}"
-    private val email = name + "@example.com"
+    private val signUpDescription = "Short bio containing my name to identify me: $signUpName"
+    private val signUpEmail = signUpName + "@example.com"
     private val psswd = "iLoveSwent1234!"
+    private val signInName = "testUser"
+    private val signInEmail = signInName + "@example.com"
+    private val signInDescription = "Short bio containing my name to identify me: $signInName"
   }
 
   @Before fun setUp() {}
@@ -71,7 +70,7 @@ class EndToEnd1 : TestCase() {
     composeTestRule
         .onNodeWithTag(AuthenticationScreens.EMAIL_FIELD)
         .assertIsDisplayed()
-        .performTextInput(email)
+        .performTextInput(signUpEmail)
     composeTestRule
         .onNodeWithTag(AuthenticationScreens.PASSWORD_FIELD)
         .assertIsDisplayed()
@@ -92,7 +91,7 @@ class EndToEnd1 : TestCase() {
     composeTestRule
         .onNodeWithTag(ProfileScreens.NAME_INPUT_FIELD)
         .assertIsDisplayed()
-        .performTextInput(name)
+        .performTextInput(signUpName)
     composeTestRule
         .onNodeWithTag(ProfileScreens.DOB_INPUT_FIELD)
         .assertIsDisplayed()
@@ -100,7 +99,7 @@ class EndToEnd1 : TestCase() {
     composeTestRule
         .onNodeWithTag(ProfileScreens.DESCRIPTION_INPUT_FIELD)
         .assertIsDisplayed()
-        .performTextInput(description)
+        .performTextInput(signUpDescription)
     Espresso.closeSoftKeyboard()
     composeTestRule.onNodeWithTag(ProfileScreens.SAVE_BUTTON).assertIsDisplayed().performClick()
     composeTestRule.waitUntil(1000) {
@@ -113,15 +112,69 @@ class EndToEnd1 : TestCase() {
     composeTestRule
         .onNodeWithTag(ProfileScreen.NAME_FIELD)
         .assertIsDisplayed()
-        .assertTextEquals(name)
+        .assertTextEquals(signUpName)
     composeTestRule
         .onNodeWithTag(ProfileScreen.DESCRIPTION_FIELD)
         .assertIsDisplayed()
-        .assertTextEquals(description)
+        .assertTextEquals(signUpDescription)
     composeTestRule.onNodeWithTag(ProfileScreen.NO_REVIEWS_CARD).assertIsDisplayed()
     composeTestRule
         .onNodeWithTag(ProfileScreen.CONTRIBUTION_FIELD)
         .assertIsDisplayed()
         .assertTextEquals("New user")
+  }
+
+  @Test
+  fun signInEndToEnd() {
+
+    composeTestRule.setContent { MainActivity() }
+
+    // SignIn Screen
+    composeTestRule.waitForIdle()
+    Log.d(TAG, "User arrives on SignIn Screen")
+    composeTestRule
+        .onNodeWithTag(AuthenticationScreens.EMAIL_FIELD)
+        .assertIsDisplayed()
+        .performTextInput(signInEmail)
+    composeTestRule
+        .onNodeWithTag(AuthenticationScreens.PASSWORD_FIELD)
+        .assertIsDisplayed()
+        .performTextInput(psswd)
+    Espresso.closeSoftKeyboard()
+    composeTestRule.onNodeWithTag(SignInScreen.SIGN_IN_BUTTON).assertIsDisplayed().performClick()
+    composeTestRule.waitUntil(1000) {
+      composeTestRule.onAllNodesWithTag(ProfileScreen.SCREEN).fetchSemanticsNodes().size == 1
+    }
+
+    // Profile Screen
+    composeTestRule.waitForIdle()
+    composeTestRule.waitUntil(
+        1000) { // need to wait because very first recomposition has not fetched data yet
+          try { // trick waitUntil into thinking this counts as a SemanticNodeInteraction
+            composeTestRule
+                .onNodeWithTag(ProfileScreen.NAME_FIELD)
+                .assertIsDisplayed()
+                .assertTextEquals(signInName)
+            true
+          } catch (e: AssertionError) {
+            false
+          }
+        }
+    Log.d(TAG, "User arrives on Profile Screen")
+    composeTestRule
+        .onNodeWithTag(ProfileScreen.NAME_FIELD)
+        .assertIsDisplayed()
+        .assertTextEquals(signInName)
+    composeTestRule
+        .onNodeWithTag(ProfileScreen.DESCRIPTION_FIELD)
+        .assertIsDisplayed()
+        .assertTextEquals(signInDescription)
+    composeTestRule
+        .onNodeWithTag(ProfileScreen.NO_REVIEWS_CARD)
+        .assertIsDisplayed() // TODO: change once implemented the reviews
+    composeTestRule
+        .onNodeWithTag(ProfileScreen.CONTRIBUTION_FIELD)
+        .assertIsDisplayed()
+        .assertTextEquals("New user") // TODO: change once implemented the statuses
   }
 }
