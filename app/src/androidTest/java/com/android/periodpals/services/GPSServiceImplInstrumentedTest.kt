0c3d6ea388e13b4ba8b7521836b3e3bc4cb95697
Bruno Lazarini Sigg <bruno.lazarinisigg@epfl.ch>
2024-11-14 20:18:06 +0100

    test: all together

--- app/src/androidTest/java/com/android/periodpals/services/GPSServiceImplInstrumentedTest.kt
+++ app/src/androidTest/java/com/android/periodpals/services/GPSServiceImplInstrumentedTest.kt
@@ -55,7 +55,6 @@ class GPSServiceImplInstrumentedTest {
     gpsService.cleanup()
   }
 
-  /*
   @Test
   fun testShouldReceiveLocationUpdate() = runTest {
     // Start location updates
@@ -70,7 +69,7 @@ class GPSServiceImplInstrumentedTest {
     // The received location should be different than the default
     assertNotEquals(defaultLat, updatedLocation.lat)
     assertNotEquals(defaultLong, updatedLocation.long)
-  }*/
+  }
 
   @Test
   fun testSwitchingLocationAccuracy() = runTest {
@@ -100,7 +99,6 @@ class GPSServiceImplInstrumentedTest {
     assertNotEquals(approxLocation.long, preciseLocation.long)
   }
 
-  /*
   @Test
   fun testCleanupStopsUpdates() = runTest {
     // Start updates
@@ -121,5 +119,5 @@ class GPSServiceImplInstrumentedTest {
     // Location shouldn't have changed
     assert(updatedLocation.lat == finalLocation.lat)
     assert(updatedLocation.long == finalLocation.long)
-  }*/
+  }
 }
