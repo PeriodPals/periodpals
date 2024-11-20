18c8342e9151df4956afb3ddbc920608b8b27262
Harrishan <harrishan.raveendran@epfl.ch>
2024-11-20 10:31:16 +0100

    feat: add row to manage user notification in `SettingsScreen`

--- app/src/main/java/com/android/periodpals/ui/settings/SettingsScreen.kt
+++ app/src/main/java/com/android/periodpals/ui/settings/SettingsScreen.kt
@@ -2,13 +2,21 @@ package com.android.periodpals.ui.settings
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
@@ -23,6 +31,7 @@ private const val SCREEN_TITLE = "My Settings"
 
 @Composable
 fun SettingsScreen(userViewModel: UserViewModel, navigationActions: NavigationActions) {
+  var receiveNotifications by remember { mutableStateOf(true) }
   Scaffold(
       modifier = Modifier.fillMaxSize().testTag(EditProfileScreen.SCREEN),
       topBar = {
@@ -47,6 +56,12 @@ fun SettingsScreen(userViewModel: UserViewModel, navigationActions: NavigationAc
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement =
             Arrangement.spacedBy(MaterialTheme.dimens.small2, Alignment.CenterVertically),
-    ) {}
+    ) {
+      Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+        Text("Receive Pals’ Notifications")
+        Switch(checked = receiveNotifications, onCheckedChange = { receiveNotifications = it })
+      }
+      Text("Notify me when a pal needs ...")
+    }
   }
 }
