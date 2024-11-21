19e17cd77a43aaa3d30f45e87cb771ba2015ab87
francelu <france.lu@epfl.ch>
2024-11-21 18:39:46 +0100

    feat: add settings button to `ProfileScreen` and tests

---
+++ app/src/main/java/com/android/periodpals/ui/settings/SettingsScreen.kt
@@ -0,0 +1,43 @@
+package com.android.periodpals.ui.settings
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import com.android.periodpals.resources.C.Tag
+import com.android.periodpals.ui.navigation.NavigationActions
+import com.android.periodpals.ui.theme.dimens
+
+/** TODO: Placeholder Screen, waiting for implementation */
+@Composable
+fun SettingsScreen(navigationActions: NavigationActions) {
+  Scaffold(
+      modifier = Modifier.fillMaxSize().testTag(Tag.TimerScreen.SCREEN),
+  ) { paddingValues ->
+    Column(
+        modifier =
+            Modifier.fillMaxSize()
+                .padding(paddingValues)
+                .padding(
+                    horizontal = MaterialTheme.dimens.medium3,
+                    vertical = MaterialTheme.dimens.small3,
+                )
+                .verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement =
+            Arrangement.spacedBy(MaterialTheme.dimens.small2, Alignment.CenterVertically),
+    ) {
+      // TODO: delete when implementing the screen
+      Text("Settings Screen", modifier = Modifier.fillMaxSize().testTag(Tag.TimerScreen.TIMER_TEXT))
+    }
+  }
+}
