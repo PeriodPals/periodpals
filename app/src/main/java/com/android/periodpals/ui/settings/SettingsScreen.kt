56c6b5d84f4849a016f3fa10cfddd7aca0c45768
Harrishan <harrishan.raveendran@epfl.ch>
2024-11-21 05:29:42 +0100

    feat: add a dialog that asks the user whether they really want to delete their account

--- app/src/main/java/com/android/periodpals/ui/settings/SettingsScreen.kt
+++ app/src/main/java/com/android/periodpals/ui/settings/SettingsScreen.kt
@@ -1,6 +1,7 @@
 package com.android.periodpals.ui.settings
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,9 +9,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.Logout
@@ -19,6 +22,10 @@ import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.material.icons.outlined.Key
 import androidx.compose.material.icons.outlined.LightMode
 import androidx.compose.material.icons.outlined.PhoneAndroid
+import androidx.compose.material.icons.outlined.SentimentVeryDissatisfied
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
@@ -39,11 +46,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import com.android.periodpals.model.user.UserViewModel
 import com.android.periodpals.resources.C.Tag.ProfileScreens.EditProfileScreen
+import com.android.periodpals.resources.ComponentColor.getFilledPrimaryButtonColors
 import com.android.periodpals.resources.ComponentColor.getMenuItemColors
 import com.android.periodpals.resources.ComponentColor.getMenuTextFieldColors
 import com.android.periodpals.resources.ComponentColor.getSwitchColors
+import com.android.periodpals.resources.ComponentColor.getTertiaryCardColors
 import com.android.periodpals.ui.navigation.NavigationActions
 import com.android.periodpals.ui.navigation.Screen
 import com.android.periodpals.ui.navigation.TopAppBar
@@ -61,6 +72,11 @@ fun SettingsScreen(userViewModel: UserViewModel, navigationActions: NavigationAc
   var expanded by remember { mutableStateOf(false) }
   var theme by remember { mutableStateOf("System") }
   var icon by remember { mutableStateOf(Icons.Outlined.PhoneAndroid) }
+  var showDialog by remember { mutableStateOf(false) }
+
+  if (showDialog) {
+    DeleteAccountDialog(navigationActions, onDismiss = { showDialog = false })
+  }
   Scaffold(
       modifier = Modifier.fillMaxSize().testTag(EditProfileScreen.SCREEN),
       topBar = {
@@ -285,7 +301,7 @@ fun SettingsScreen(userViewModel: UserViewModel, navigationActions: NavigationAc
               style = MaterialTheme.typography.labelLarge,
               modifier = Modifier.wrapContentHeight(),
               color = MaterialTheme.colorScheme.onSurface)
-          Icon(Icons.Outlined.Key, contentDescription = null)
+          Icon(Icons.Outlined.Key, contentDescription = null, modifier = Modifier.clickable {})
         }
         Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
           Text(
@@ -293,7 +309,10 @@ fun SettingsScreen(userViewModel: UserViewModel, navigationActions: NavigationAc
               style = MaterialTheme.typography.labelLarge,
               modifier = Modifier.wrapContentHeight(),
               color = MaterialTheme.colorScheme.onSurface)
-          Icon(Icons.AutoMirrored.Outlined.Logout, contentDescription = null)
+          Icon(
+              Icons.AutoMirrored.Outlined.Logout,
+              contentDescription = null,
+              modifier = Modifier.clickable { navigationActions.navigateTo(Screen.SIGN_IN) })
         }
         Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
           Text(
@@ -305,9 +324,72 @@ fun SettingsScreen(userViewModel: UserViewModel, navigationActions: NavigationAc
           Icon(
               Icons.Outlined.Delete,
               contentDescription = null,
-              tint = MaterialTheme.colorScheme.error)
+              tint = MaterialTheme.colorScheme.error,
+              modifier = Modifier.clickable { showDialog = true })
         }
       }
     }
   }
+}
+
+/**
+ * Composable function that displays a dialog asking for the user whether they want to delete their
+ * account..
+ *
+ * @param navigationActions The navigation actions to use.
+ * @param onDismiss The callback to dismiss the dialog.
+ */
+@Composable
+private fun DeleteAccountDialog(navigationActions: NavigationActions, onDismiss: () -> Unit) {
+
+  Dialog(
+      onDismissRequest = onDismiss,
+      properties = DialogProperties(usePlatformDefaultWidth = false)) {
+        Card(
+            modifier = Modifier.wrapContentSize(),
+            shape = RoundedCornerShape(size = MaterialTheme.dimens.cardRoundedSize),
+            colors = getTertiaryCardColors(),
+            elevation =
+                CardDefaults.cardElevation(defaultElevation = MaterialTheme.dimens.cardElevation),
+        ) {
+          Column(
+              modifier = Modifier.wrapContentSize().padding(MaterialTheme.dimens.small2),
+              horizontalAlignment = Alignment.CenterHorizontally,
+              verticalArrangement =
+                  Arrangement.spacedBy(MaterialTheme.dimens.small2, Alignment.CenterVertically),
+          ) {
+            Icon(
+                modifier = Modifier.size(MaterialTheme.dimens.iconSize),
+                imageVector = Icons.Outlined.SentimentVeryDissatisfied,
+                contentDescription = "Account Deletion Emoji",
+            )
+            Text(
+                modifier = Modifier.wrapContentSize(),
+                text = "Are you sure you want to delete your account?",
+                style = MaterialTheme.typography.bodyMedium,
+                textAlign = TextAlign.Center,
+            )
+            Row {
+              Button(
+                  onClick = { navigationActions.navigateTo(Screen.SIGN_IN) },
+                  colors = getFilledPrimaryButtonColors(),
+                  modifier = Modifier.padding(MaterialTheme.dimens.small2)) {
+                    Text(
+                        "Yes",
+                        style = MaterialTheme.typography.labelLarge,
+                        color = MaterialTheme.colorScheme.onSecondary)
+                  }
+              Button(
+                  onClick = onDismiss,
+                  colors = getFilledPrimaryButtonColors(),
+                  modifier = Modifier.padding(MaterialTheme.dimens.small2)) {
+                    Text(
+                        "No",
+                        style = MaterialTheme.typography.labelLarge,
+                        color = MaterialTheme.colorScheme.onSecondary)
+                  }
+            }
+          }
+        }
+      }
 }
