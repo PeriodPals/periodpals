82a16c5e4c0a00a9389ec55b6f580977f7be8d5e
taghizadlaura <laura.taghizad@epfl.ch>
2024-11-21 17:41:34 +0100

    style: remove spacer

--- app/src/main/java/com/android/periodpals/ui/timer/Timer.kt
+++ app/src/main/java/com/android/periodpals/ui/timer/Timer.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -152,8 +151,6 @@ fun TimerScreen(
                   style = MaterialTheme.typography.headlineSmall)
             }
       }
-
-      Spacer(Modifier.size(MaterialTheme.dimens.small1))
 
       // Useful tip
       UsefulTip()
