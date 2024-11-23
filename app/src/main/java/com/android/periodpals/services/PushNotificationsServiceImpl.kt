ea166110e5c27f761122b578eb61e050d1a922b5
charlie mangano <eva.mangano@epfl.ch>
2024-11-23 16:17:09 +0100

    fix: change `requestPermissionLauncher` type to initialise lazily
    Modified the `requestPermissionLauncher` in `PushNotificationsServiceImpl` from an `ActivityResultLauncher<String>` to a  `(ComponentActivity) -> ActivityResultLauncher<String>` so that it initialises lazily.

--- app/src/main/java/com/android/periodpals/services/PushNotificationsServiceImpl.kt
+++ app/src/main/java/com/android/periodpals/services/PushNotificationsServiceImpl.kt
@@ -32,28 +32,34 @@ class PushNotificationsServiceImpl(private val activity: ComponentActivity) :
     Log.d(TAG, "Refreshed token: $token")
   }
 
-  private val requestPermissionLauncher =
-      activity.registerForActivityResult(
-          ActivityResultContracts.RequestPermission(),
-      ) { isGranted: Boolean ->
-        if (isGranted) {
-          Log.d(TAG, "Notification permission granted")
-          Toast.makeText(activity, "Notification permission granted", Toast.LENGTH_SHORT).show()
-          _pushPermissionsGranted.value = true
-        } else {
-          Log.d(TAG, "Notification permission denied")
-          Toast.makeText(activity, "Notification permission denied", Toast.LENGTH_SHORT).show()
-          _pushPermissionsGranted.value = false
-        }
+  // needs to be a `(ComponentActivity) -> ActivityResultLauncher<String>` so that it won't
+  // initialise if it's not being called (lazy)
+  private val requestPermissionLauncher = { activity: ComponentActivity ->
+    Log.d(TAG, "Creating permission launcher")
+    activity.registerForActivityResult(
+        ActivityResultContracts.RequestPermission(),
+    ) { isGranted: Boolean ->
+      if (isGranted) {
+        Log.d(TAG, "Notification permission granted")
+        Toast.makeText(activity, "Notification permission granted", Toast.LENGTH_SHORT).show()
+        _pushPermissionsGranted.value = true
+      } else {
+        Log.d(TAG, "Notification permission denied")
+        Toast.makeText(activity, "Notification permission denied", Toast.LENGTH_SHORT).show()
+        _pushPermissionsGranted.value = false
       }
+    }
+  }
 
   /** Asks the user for permission to send push notifications. */
   override fun askPermission() {
     // no need to ask for permission on API level < 33 (TIRAMISU)
     if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+      Log.d(TAG, "Notification permission not needed on API level < 33")
       _pushPermissionsGranted.value = true
       return
     }
+    Log.d(TAG, "Checking notification permission")
 
     _pushPermissionsGranted.value =
         ContextCompat.checkSelfPermission(activity, Manifest.permission.POST_NOTIFICATIONS) ==
@@ -68,6 +74,6 @@ class PushNotificationsServiceImpl(private val activity: ComponentActivity) :
     //       Manifest.permission.POST_NOTIFICATIONS)
 
     Log.d(TAG, "Requesting notification permission")
-    requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+    requestPermissionLauncher(activity).launch(Manifest.permission.POST_NOTIFICATIONS)
   }
 }
