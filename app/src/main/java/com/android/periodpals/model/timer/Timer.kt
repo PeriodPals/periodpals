9c2da8648b649a91354b9827a67b2c2a21f40c6e
francelu <france.lu@epfl.ch>
2024-11-25 16:19:38 +0100

    fix: remove unused `TimerStatus` and `DEFAULT_TIMER`

--- app/src/main/java/com/android/periodpals/model/timer/Timer.kt
+++ app/src/main/java/com/android/periodpals/model/timer/Timer.kt
@@ -14,17 +14,3 @@ data class Timer(val lastTimers: List<Long>) {
     return TimerDto(lastTimers = Json.encodeToString(this.lastTimers))
   }
 }
-
-// TODO: update timer status for the reminders
-/**
- * Enum class representing the current status of the timer.
- *
- * @property RUNNING The timer is currently running.
- * @property STOPPED The timer has been stopped, not yet started.
- */
-enum class TimerStatus {
-  RUNNING,
-  STOPPED
-}
-
-val DEFAULT_TIMER = Timer(lastTimers = emptyList())
