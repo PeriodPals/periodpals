d4dbe2bbb9af158306aa65d687e7b3d02d60ef8a
Bruno Lazarini Sigg <bruno.lazarinisigg@epfl.ch>
2024-11-25 10:03:20 +0100

    feat: remove serialization methods as this is best done in a new PR

--- app/src/main/java/com/android/periodpals/model/location/GPSLocation.kt
+++ app/src/main/java/com/android/periodpals/model/location/GPSLocation.kt
@@ -2,10 +2,6 @@ package com.android.periodpals.model.location
 
 import org.osmdroid.util.GeoPoint
 
-private const val PARTS_ERROR_MESSAGE = "Invalid format. Expected 'lat,long'."
-private const val PARSE_ERROR_MESSAGE = "Invalid numeric values for latitude and longitude"
-private const val STRING_DELIMITER = ","
-
 /**
  * Represents a geographic location defined by latitude and longitude coordinates
  *
@@ -22,43 +18,6 @@ data class GPSLocation(val lat: Double, val long: Double) {
    * @return The respective [GeoPoint].
    */
   fun toGeoPoint() = GeoPoint(lat, long)
-
-  /**
-   * Transform this location into a [Location]
-   *
-   * @return The respective [Location]
-   */
-  fun toLocation() = Location(lat, long, CURRENT_LOCATION_NAME)
-
-  /**
-   * Converts the GPSLocation object to a String in "lat,long" format.
-   *
-   * @return A String representation of the GPSLocation object.
-   */
-  override fun toString(): String = "$lat,$long"
-
-  /**
-   * Converts a String in "lat,long" format back to a GPSLocation object.
-   *
-   * @param value The [String] representation of the [GPSLocation] object.
-   * @return The [GPSLocation] parsed from the string.
-   * @throws IllegalArgumentException if the string format or the numeric values for the coordinates
-   *   are invalid
-   */
-  fun fromString(value: String): GPSLocation {
-    val parts = value.split(STRING_DELIMITER)
-    if (parts.size != 2) {
-      throw IllegalArgumentException(PARTS_ERROR_MESSAGE)
-    }
-    val lat = parts[0].toDoubleOrNull()
-    val long = parts[1].toDoubleOrNull()
-
-    if (lat == null || long == null) {
-      throw IllegalArgumentException(PARSE_ERROR_MESSAGE)
-    }
-
-    return GPSLocation(lat, long)
-  }
 
   /**
    * Default location to be used when the user has not granted permission to access their location.
