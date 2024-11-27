320ea47d633c830a598fac39a7a065873d51d91a
francelu <france.lu@epfl.ch>
2024-11-27 18:33:07 +0100

    feat: timer data class and dto only contain `time` property

--- app/src/main/java/com/android/periodpals/model/timer/Timer.kt
+++ app/src/main/java/com/android/periodpals/model/timer/Timer.kt
@@ -3,12 +3,6 @@ package com.android.periodpals.model.timer
 /**
  * Data class representing a timer.
  *
- * @property timerID The unique identifier of the timer, given when created in Supabase.
- * @property userID The user ID associated with the timer.
  * @property time The time of the timer.
  */
-data class Timer(
-    val timerID: String?, // given when created in supabase
-    val userID: String,
-    val time: Long
-)
+data class Timer(val time: Long)
--- app/src/main/java/com/android/periodpals/model/timer/TimerDto.kt
+++ app/src/main/java/com/android/periodpals/model/timer/TimerDto.kt
@@ -6,24 +6,16 @@ import kotlinx.serialization.Serializable
 /**
  * Data Transfer Object (DTO) for timer data.
  *
- * @property timerID The unique identifier of the timer, given when created in Supabase.
- * @property userID The user ID associated with the timer.
  * @property time The time of the timer.
  */
 @Serializable
-data class TimerDto(
-    @SerialName("timerID") val timerID: String?,
-    @SerialName("userID") val userID: String,
-    @SerialName("time") val time: Long
-) {
+data class TimerDto(@SerialName("time") val time: Long) {
   /**
    * Constructs a `TimerDto` from a `Timer` object.
    *
    * @param timer The `Timer` object to be converted into a `TimerDto`.
    */
-  constructor(
-      timer: Timer
-  ) : this(timerID = timer.timerID, userID = timer.userID, time = timer.time)
+  constructor(timer: Timer) : this(time = timer.time)
 
   /**
    * Converts this `TimerDto` to a `Timer` object.
@@ -31,6 +23,6 @@ data class TimerDto(
    * @return The `Timer` object created from this `TimerDto`.
    */
   fun toTimer(): Timer {
-    return Timer(timerID = timerID, userID = userID, time = time)
+    return Timer(time = time)
   }
 }
