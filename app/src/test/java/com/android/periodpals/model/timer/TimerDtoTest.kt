42ea0036f3b3adea80dfa78ee3686ffe59cb49b5
francelu <france.lu@epfl.ch>
2024-11-27 15:55:11 +0100

    fix: adapt `TimerDtoTest` and add Robolectric

--- app/src/test/java/com/android/periodpals/model/timer/TimerDtoTest.kt
+++ app/src/test/java/com/android/periodpals/model/timer/TimerDtoTest.kt
@@ -1,35 +1,26 @@
 package com.android.periodpals.model.timer
 
 import junit.framework.Assert.assertEquals
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
 import org.junit.Test
 
 class TimerDtoTest {
   companion object {
-    val timer1 = Timer(lastTimers = listOf(4, 5, 6))
-    val timerDto1 = TimerDto(lastTimers = Json.encodeToString(listOf(4, 5, 6)))
+    val timer1 = Timer(timerID = "1", userID = "2", time = 3)
+    val timerDto1 = TimerDto(timerID = "1", userID = "2", time = 3)
 
-    val timer2 = Timer(lastTimers = listOf(5, 3, 89))
-    val timerDto2 = TimerDto(lastTimers = Json.encodeToString(listOf(5, 3, 89)))
+    val timer2 = Timer(timerID = "4", userID = "5", time = 6)
+    val timerDto2 = TimerDto(timerID = "4", userID = "5", time = 6)
   }
 
   @Test
-  fun asTimerIsCorrect() {
-    assertEquals(timer1, timerDto1.asTimer())
-    assertEquals(timer2, timerDto2.asTimer())
+  fun constructorIsCorrect() {
+    assertEquals(timerDto1, TimerDto(timer1))
+    assertEquals(timerDto2, TimerDto(timer2))
   }
 
   @Test
-  fun asTimerDtoIsCorrect() {
-    assertEquals(timerDto1, timer1.asTimerDto())
-    assertEquals(timerDto2, timer2.asTimerDto())
-  }
-
-  @Test
-  fun asTimerHandlesEmptyList() {
-    val timerDto = TimerDto(lastTimers = Json.encodeToString(emptyList<Long>()))
-    val timer = timerDto.asTimer()
-    assertEquals(emptyList<Int>(), timer.lastTimers)
+  fun toTimerIsCorrect() {
+    assertEquals(timer1, timerDto1.toTimer())
+    assertEquals(timer2, timerDto2.toTimer())
   }
 }
