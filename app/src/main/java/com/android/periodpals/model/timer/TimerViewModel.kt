36bb673fb51366b1e8d9f562dac6976e6166ce4a
francelu <france.lu@epfl.ch>
2024-12-02 00:56:33 +0100

    feat: update timer VM to handle average, remaining time, if timer is running

--- app/src/main/java/com/android/periodpals/model/timer/TimerViewModel.kt
+++ app/src/main/java/com/android/periodpals/model/timer/TimerViewModel.kt
@@ -1,8 +1,14 @@
 package com.android.periodpals.model.timer
 
 import android.util.Log
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import java.util.Date
+import java.util.TimerTask
 import kotlinx.coroutines.launch
 
 private const val TAG = "TimerViewModel"
@@ -17,9 +23,31 @@ class TimerViewModel(
     private val timerRepository: TimerRepository,
     private val timerManager: TimerManager
 ) : ViewModel() {
-  private var _userTimersList = mutableListOf<Timer>()
-  val userTimersList: List<Timer>
-    get() = _userTimersList
+  private var _userAverageTimer = mutableDoubleStateOf(0.0)
+  val userAverageTimer: MutableState<Double> = _userAverageTimer
+
+  private val timer = java.util.Timer()
+  private val _remainingTime = MutableLiveData(COUNTDOWN_6_HOURS)
+  val remainingTime: LiveData<Long>
+    get() = _remainingTime
+
+  private inner class TimeTask : TimerTask() {
+    override fun run() {
+      if (timerManager.timerCounting()) {
+        val currentTime = Date().time
+        val startTimeMillis = timerManager.startTime()!!.time
+        _remainingTime.postValue(COUNTDOWN_6_HOURS - (currentTime - startTimeMillis))
+
+        Log.d(TAG, "run: remaining time: ${_remainingTime.value}")
+      } else {
+        _remainingTime.postValue(COUNTDOWN_6_HOURS)
+      }
+    }
+  }
+
+  init {
+    timer.schedule(TimeTask(), 0, 500)
+  }
 
   /**
    * Starts the timer.
@@ -70,45 +98,50 @@ class TimerViewModel(
   /**
    * Stops the timer and saves the elapsed time to the database (Supabase).
    *
+   * @param userID The ID of the user whose timer is to be stopped.
    * @param onSuccess Callback function to be called when the timer is successfully stopped.
    * @param onFailure Callback function to be called when there is an error stopping the timer.
    */
   fun stopTimer(
+      userID: String,
       onSuccess: () -> Unit = { Log.d(TAG, "stopTimer success callback") },
       onFailure: (Exception) -> Unit = { e: Exception ->
         Log.d(TAG, "stopTimer failure callback: $e")
       }
   ) {
-    var elapsedTime = 0L
     timerManager.stopTimerAction(
-        onSuccess = { elapsedTime = it },
+        onSuccess = { time ->
+          Log.d(TAG, "stopTimer: success callback")
+          viewModelScope.launch {
+            timerRepository.addTimer(
+                timerDto = TimerDto(Timer(time = time)),
+                onSuccess = {
+                  Log.d(TAG, "stopTimer: success callback")
+                  onSuccess()
+                },
+                onFailure = { e ->
+                  Log.d(TAG, "stopTimer: fail to create timer: ${e.message}")
+                  onFailure(e)
+                },
+            )
+          }
+          computeAverageTime(userID, onSuccess, onFailure)
+        },
         onFailure = { e: Exception ->
           Log.d(TAG, "stopTimer: fail to stop timer: ${e.message}")
           onFailure(e)
-        })
-
-    viewModelScope.launch {
-      timerRepository.addTimer(
-          timerDto = TimerDto(Timer(time = elapsedTime)),
-          onSuccess = {
-            Log.d(TAG, "stopTimer: Success")
-            onSuccess()
-          },
-          onFailure = { e ->
-            Log.d(TAG, "stopTimer: fail to create timer: ${e.message}")
-            onFailure(e)
-          })
-    }
+        },
+    )
   }
 
   /**
-   * Fetches all the timers of a user from the database.
+   * Fetches the timers of a user and computes the average time.
    *
    * @param userID The ID of the user whose timers are to be fetched.
    * @param onSuccess Callback function to be called when the timers are successfully fetched.
    * @param onFailure Callback function to be called when there is an error fetching the timers.
    */
-  fun fetchTimersOfUser(
+  fun computeAverageTime(
       userID: String,
       onSuccess: () -> Unit = { Log.d(TAG, "updateOverallAverageTime success callback") },
       onFailure: (Exception) -> Unit = { e: Exception ->
@@ -118,21 +151,24 @@ class TimerViewModel(
     viewModelScope.launch {
       timerRepository.getTimersOfUser(
           userID = userID,
-          onSuccess = {
-            Log.d(TAG, "updateOverallAverageTime: Success")
-            _userTimersList = it.toMutableList()
+          onSuccess = { timerList ->
+            Log.d(TAG, "updateOverallAverageTime: success callback")
+            _userAverageTimer.doubleValue = timerList.map { it.time }.average()
             onSuccess()
           },
           onFailure = { e ->
             Log.d(TAG, "updateOverallAverageTime: fail to create timer: ${e.message}")
-            _userTimersList = mutableListOf()
             onFailure(e)
           })
     }
   }
 
-  /** Retrieves the remaining time on the timer. */
-  fun getRemainingTime(): Long {
-    return timerManager.getRemainingTime()
+  /**
+   * Checks if the timer is running.
+   *
+   * @return True if the timer is running, false otherwise.
+   */
+  fun timerRunning(): Boolean {
+    return timerManager.timerCounting()
   }
 }
