a25dc58d67f4b9266bda83b14eed454471f427a6
Alonso <alonso.coaguila@epfl.ch>
2024-12-06 00:00:05 +0100

    feat: add `UserModelPowerSync` which should replace `UserModelSupabase` to enable a local cache of profile

---
+++ app/src/main/java/com/android/periodpals/model/user/UserModelPowerSync.kt
@@ -0,0 +1,56 @@
+package com.android.periodpals.model.user
+
+import android.util.Log
+import com.powersync.PowerSyncDatabase
+import com.powersync.connector.supabase.SupabaseConnector
+
+private const val TAG = "UserModelPowerSync"
+private const val USERS = "users"
+
+class UserModelPowerSync(
+    private val db: PowerSyncDatabase
+) : UserRepository{
+    override suspend fun loadUserProfile(
+        onSuccess: (UserDto) -> Unit,
+        onFailure: (Exception) -> Unit
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun createUserProfile(
+        user: User,
+        onSuccess: () -> Unit,
+        onFailure: (Exception) -> Unit
+    ) {
+        try {
+            db.writeTransaction { tx ->
+                tx.execute(
+                    " INSERT INTO $USERS (name, imageUrl, description, dob) VALUES (?, ?, ?, ?);",
+                    user.asList()
+                )
+            }
+            Log.d(TAG, "createUserProfile: Success")
+            onSuccess()
+        } catch (e: Exception) {
+            Log.d(TAG, "createUserProfile: fail to create user profile: ${e.message}")
+            onFailure(e)
+        }
+    }
+
+    override suspend fun upsertUserProfile(
+        userDto: UserDto,
+        onSuccess: (UserDto) -> Unit,
+        onFailure: (Exception) -> Unit
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteUserProfile(
+        idUser: String,
+        onSuccess: () -> Unit,
+        onFailure: (Exception) -> Unit
+    ) {
+        TODO("Not yet implemented")
+    }
+
+}
