3cd4e04eadf6a2ae3786f111f2b0e19cba3956fd
francelu <france.lu@epfl.ch>
2024-12-07 23:48:04 +0100

    feat: add timer id and instruction text to data class and dto

--- app/src/test/java/com/android/periodpals/model/timer/TimerDtoTest.kt
+++ app/src/test/java/com/android/periodpals/model/timer/TimerDtoTest.kt
@@ -5,11 +5,17 @@ import org.junit.Test
 
 class TimerDtoTest {
   companion object {
-    val timer1 = Timer(time = 3)
-    val timerDto1 = TimerDto(time = 3)
+    private const val TIME_1 = 3L
+    private const val INSTRUCTION_1 = "Timer 1"
+    val timer1 = Timer(time = TIME_1, instructionText = INSTRUCTION_1)
+    val timerDto1 = TimerDto(id = timer1.id, time = TIME_1, instructionText = INSTRUCTION_1)
+    val id1 = "someId1"
 
-    val timer2 = Timer(time = 6)
-    val timerDto2 = TimerDto(time = 6)
+    private const val TIME_2 = 6L
+    private const val INSTRUCTION_2 = "Timer 2"
+    val timer2 = Timer(time = TIME_2, instructionText = INSTRUCTION_2)
+    val timerDto2 = TimerDto(id = timer2.id, time = TIME_2, instructionText = INSTRUCTION_2)
+    val id2 = "someId2"
   }
 
   @Test
@@ -20,7 +26,7 @@ class TimerDtoTest {
 
   @Test
   fun toTimerIsCorrect() {
-    assertEquals(timer1, timerDto1.toTimer())
-    assertEquals(timer2, timerDto2.toTimer())
+    assertEquals(timer1.copy(id = id1), timerDto1.copy(id = id1).toTimer())
+    assertEquals(timer2.copy(id = id2), timerDto2.copy(id = id2).toTimer())
   }
 }
