c2a9ae4890972a8e00e8e87f2f8749937fd3b507
Alonso <alonso.coaguila@epfl.ch>
2024-12-10 21:08:51 +0100

    style: ktfmt fortnite

--- app/src/main/java/com/android/periodpals/model/user/UserModelPowerSync.kt
+++ app/src/main/java/com/android/periodpals/model/user/UserModelPowerSync.kt
@@ -2,122 +2,121 @@ package com.android.periodpals.model.user
 
 import android.util.Log
 import com.powersync.PowerSyncDatabase
-import com.powersync.connector.supabase.SupabaseConnector
 import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.auth.auth
-import io.github.jan.supabase.auth.user.UserInfo
 
 private const val TAG = "UserModelPowerSync"
 private const val USERS = "users"
 
-class UserModelPowerSync(
-    private val db: PowerSyncDatabase,
-    private val supabase: SupabaseClient
-) : UserRepository{
+class UserModelPowerSync(private val db: PowerSyncDatabase, private val supabase: SupabaseClient) :
+    UserRepository {
 
-    override suspend fun loadUserProfile(
-        onSuccess: (UserDto) -> Unit,
-        onFailure: (Exception) -> Unit
-    ) {
-        try {
-            val currUser = supabase.auth.currentUserOrNull()?.id ?: throw Exception("Supabase does not have a user logged in")
-            var user: UserDto? = null
+  override suspend fun loadUserProfile(
+      onSuccess: (UserDto) -> Unit,
+      onFailure: (Exception) -> Unit
+  ) {
+    try {
+      val currUser =
+          supabase.auth.currentUserOrNull()?.id
+              ?: throw Exception("Supabase does not have a user logged in")
+      var user: UserDto? = null
 
-            db.writeTransaction { tx ->
-                user = tx.getOptional (
-                    "SELECT name, imageUrl, description, dob from $USERS where user_id = ?",
-                    listOf(currUser)
-                ) {
-                    UserDto(
-                        name = it.getString(0)!!,
-                        imageUrl = it.getString(1)!!,
-                        description = it.getString(2)!!,
-                        dob = it.getString(3)!!
-                    )
+      db.writeTransaction { tx ->
+        user =
+            tx.getOptional(
+                "SELECT name, imageUrl, description, dob FROM $USERS WHERE user_id = ?",
+                listOf(currUser)) {
+                  UserDto(
+                      name = it.getString(0)!!,
+                      imageUrl = it.getString(1)!!,
+                      description = it.getString(2)!!,
+                      dob = it.getString(3)!!)
                 }
-            }
-            if (user == null) {
-                throw Exception("PowerSync failure did not fetch correctly")
-            }
+      }
+      if (user == null) {
+        throw Exception("PowerSync failure did not fetch correctly")
+      }
 
-            Log.d(TAG, "loadUserProfile: Success")
-            onSuccess(user!!)
-        } catch (e: Exception) {
-            Log.d(TAG, "loadUserProfile: fail to load user profile: ${e.message}")
-            onFailure(e)
-        }
+      Log.d(TAG, "loadUserProfile: Success")
+      onSuccess(user!!)
+    } catch (e: Exception) {
+      Log.d(TAG, "loadUserProfile: fail to load user profile: ${e.message}")
+      onFailure(e)
     }
+  }
 
-    override suspend fun createUserProfile(
-        user: User,
-        onSuccess: () -> Unit,
-        onFailure: (Exception) -> Unit
-    ) {
-        try {
-            db.writeTransaction { tx ->
-                tx.execute(
-                    "INSERT INTO $USERS (name, imageUrl, description, dob) VALUES (?, ?, ?, ?);",
-                    user.asList()
-                )
-            }
-            Log.d(TAG, "createUserProfile: Success")
-            onSuccess()
-        } catch (e: Exception) {
-            Log.d(TAG, "createUserProfile: fail to create user profile: ${e.message}")
-            onFailure(e)
-        }
+  override suspend fun createUserProfile(
+      user: User,
+      onSuccess: () -> Unit,
+      onFailure: (Exception) -> Unit
+  ) {
+    try {
+      db.writeTransaction { tx ->
+        tx.execute(
+            "INSERT INTO $USERS (name, imageUrl, description, dob) VALUES (?, ?, ?, ?);",
+            user.asList())
+      }
+      Log.d(TAG, "createUserProfile: Success")
+      onSuccess()
+    } catch (e: Exception) {
+      Log.d(TAG, "createUserProfile: fail to create user profile: ${e.message}")
+      onFailure(e)
     }
+  }
 
-    override suspend fun upsertUserProfile(
-        userDto: UserDto,
-        onSuccess: (UserDto) -> Unit,
-        onFailure: (Exception) -> Unit
-    ) {
-        try {
-            val currUser: String? = supabase.auth.currentUserOrNull()?.id
+  override suspend fun upsertUserProfile(
+      userDto: UserDto,
+      onSuccess: (UserDto) -> Unit,
+      onFailure: (Exception) -> Unit
+  ) {
+    try {
+      val currUser: String? = supabase.auth.currentUserOrNull()?.id
 
-            db.writeTransaction { tx ->
-                tx.execute(
-                    """
+      db.writeTransaction { tx ->
+        tx.execute(
+            """
                         INSERT INTO $USERS (user_id, name, imageUrl, description, dob)
                         VALUES (?, ?, ?, ?, ?)
                         ON CONFLICT (user_id)
                         DO UPDATE SET name = ?, imageUrl = ?, description = ?, dob = ?;
                     """,
-                    listOf(
-                        currUser, userDto.name, userDto.imageUrl, userDto.description, userDto.dob,
-                        userDto.name, userDto.imageUrl, userDto.description, userDto.dob
-                    )
-                )
-            }
-            Log.d(TAG, "upsertUserProfile: Success")
-            onSuccess(userDto)
-        } catch (e: Exception) {
-            Log.d(TAG, "upsertUserProfile: fail to create user profile: ${e.message}")
-            onFailure(e)
-        }
+            listOf(
+                currUser,
+                userDto.name,
+                userDto.imageUrl,
+                userDto.description,
+                userDto.dob,
+                userDto.name,
+                userDto.imageUrl,
+                userDto.description,
+                userDto.dob))
+      }
+      Log.d(TAG, "upsertUserProfile: Success")
+      onSuccess(userDto)
+    } catch (e: Exception) {
+      Log.d(TAG, "upsertUserProfile: fail to create user profile: ${e.message}")
+      onFailure(e)
     }
+  }
 
-    override suspend fun deleteUserProfile(
-        idUser: String,
-        onSuccess: () -> Unit,
-        onFailure: (Exception) -> Unit
-    ) {
-        try {
-            val currUser = supabase.auth.currentUserOrNull()?.id ?: throw Exception("Supabase does not have a user logged in")
+  override suspend fun deleteUserProfile(
+      idUser: String,
+      onSuccess: () -> Unit,
+      onFailure: (Exception) -> Unit
+  ) {
+    try {
+      val currUser =
+          supabase.auth.currentUserOrNull()?.id
+              ?: throw Exception("Supabase does not have a user logged in")
 
-            db.writeTransaction { tx ->
-                tx.execute(
-                    "DELETE FROM $USERS WHERE user_id=?",
-                    listOf(currUser)
-                )
-            }
-            Log.d(TAG, "deleteUserProfile: Success")
-            onSuccess()
-        } catch (e: Exception) {
-            Log.d(TAG, "deleteUserProfile: fail to delete user profile: ${e.message}")
-            onFailure(e)
-        }
+      db.writeTransaction { tx ->
+        tx.execute("DELETE FROM $USERS WHERE user_id = ?", listOf(currUser))
+      }
+      Log.d(TAG, "deleteUserProfile: Success")
+      onSuccess()
+    } catch (e: Exception) {
+      Log.d(TAG, "deleteUserProfile: fail to delete user profile: ${e.message}")
+      onFailure(e)
     }
-
+  }
 }
