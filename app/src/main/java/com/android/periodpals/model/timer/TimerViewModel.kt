f8d22c44cd62a961466b04ab84088c9664b51822
francelu <france.lu@epfl.ch>
2024-12-12 14:27:27 +0100

    style: ktfmt format

--- app/src/main/java/com/android/periodpals/model/timer/TimerViewModel.kt
+++ app/src/main/java/com/android/periodpals/model/timer/TimerViewModel.kt
@@ -9,9 +9,9 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.launch
 import java.util.Date
 import java.util.TimerTask
+import kotlinx.coroutines.launch
 
 private const val TAG = "TimerViewModel"
 private const val PERIOD = 500L
