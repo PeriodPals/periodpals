f0542b334b85cbdea72b386392b2e1c3ba6c524e
francelu <france.lu@epfl.ch>
2024-12-12 15:56:35 +0100

    refactor: clean up `ChatViewModel`

--- app/src/main/java/com/android/periodpals/model/chat/ChatViewModel.kt
+++ app/src/main/java/com/android/periodpals/model/chat/ChatViewModel.kt
@@ -9,6 +9,11 @@ import io.getstream.chat.android.client.ChatClient
 
 private const val TAG = "ChatViewModel"
 
+/**
+ * View model for the chat feature.
+ *
+ * @property chatClient The client used for connecting to the Stream Chat service.
+ */
 class ChatViewModel(private val chatClient: ChatClient) : ViewModel() {
 
   /**
@@ -24,62 +29,13 @@ class ChatViewModel(private val chatClient: ChatClient) : ViewModel() {
     }
 
     val uid = authenticationViewModel.authUserData.value!!.uid
-    authenticationViewModel.createUserChatJwtToken()
-    if (authenticationViewModel.jwtToken.value == null) {
-      Log.d(TAG, "Failed to connect user: JWT token is null.")
-      return
-    }
     val token = JwtTokenService.generateStreamToken(uid)
 
     val userImage = profile.imageUrl.ifEmpty { "https://bit.ly/2TIt8NR" }
     val user =
         io.getstream.chat.android.models.User(id = uid, name = profile.name, image = userImage)
 
-    Log.d(TAG, "User id: ${user.id}")
-    Log.d(TAG, "User token: ${token}")
-
     chatClient.connectUser(user = user, token = token).enqueue()
     Log.d(TAG, "User connected successfully.")
   }
-
-  //  private var currentChannel: ChannelClient? = null
-  //
-  //  fun initializeChannel(channelId: String) {
-  //    currentChannel = chatClient.channel("messaging", channelId)
-  //    currentChannel?.watch()?.enqueue { result ->
-  //      if (result.isSuccess) {
-  //        Log.d("ChatViewModel", "Channel connected successfully.")
-  //      } else {
-  //        Log.e("ChatViewModel", "Failed to connect to channel:")
-  //      }
-  //    }
-  //  }
-  //
-  //  fun sendMessage(messageText: String) {
-  //    currentChannel?.let { channel ->
-  //      val message = Message(text = messageText)
-  //      channel.sendMessage(message).enqueue { result: Result<Message> ->
-  //        if (result.isSuccess) {
-  //          Log.d("ChatViewModel", "Message sent successfully.")
-  //        } else {
-  //          Log.e("ChatViewModel", "Failed to send message:")
-  //        }
-  //      }
-  //    } ?: Log.e("ChatViewModel", "Current channel is not initialized.")
-  //  }
-  //
-  //  fun initializeStreamChat(userId: String) {
-  //    val user = User(id = userId)
-  //    chatClient
-  //        .connectUser(
-  //            user = user, token = "your-stream-token" // You need to generate this token
-  //            )
-  //        .enqueue { result ->
-  //          if (result.isSuccess) {
-  //            // User connected successfully
-  //          } else {
-  //            // Handle error
-  //          }
-  //        }
-  //  }
 }
