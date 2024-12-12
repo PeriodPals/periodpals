6e06e410558768f45763668b9e711194dde3b28e
Alonso <alonso.coaguila@epfl.ch>
2024-12-12 13:56:23 +0100

    feat: add `Schema.kt` to represent the local db `

---
+++ app/src/main/java/com/android/periodpals/resources/Schema.kt
@@ -0,0 +1,22 @@
+package com.android.periodpals.resources
+
+import com.powersync.db.schema.Column
+import com.powersync.db.schema.Table
+
+const val USERS = "users"
+
+val users = Table(
+    USERS,
+    listOf(
+        Column.text("user_id"),
+        Column.text("name"),
+        Column.text("email"),
+        Column.text("imageUrl"),
+        Column.text("description"),
+        Column.text("dob"),
+        Column.text("id"),
+        Column.text("fcm_token"),
+        Column.integer("erred_distance"),
+        Column.text("locationGIS")
+    )
+)
