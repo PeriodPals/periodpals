e02aa1b390ab5fa33dc0a9cd9e4dcf02a362e428
francelu <france.lu@epfl.ch>
2024-12-12 18:53:58 +0100

    fix: correct onFailure with exception in JwtTokenService callbacks

--- app/src/main/java/com/android/periodpals/services/JwtTokenService.kt
+++ app/src/main/java/com/android/periodpals/services/JwtTokenService.kt
@@ -1,22 +1,33 @@
 package com.android.periodpals.services
 
+import android.util.Log
 import com.android.periodpals.BuildConfig
 import com.android.periodpals.model.timer.HOUR
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
 import java.util.Date
 
-// Replace this with your Stream secret key (should be kept secure)
+private const val TAG = "JwtTokenService"
+
+/** Service for generating JWT tokens. */
 class JwtTokenService {
 
   companion object {
+    /**
+     * Generates a Stream Chat token for the given user ID.
+     *
+     * @param uid The user ID.
+     * @param onSuccess The callback to be invoked when the token is generated successfully.
+     * @param onFailure The callback to be invoked when the token generation fails.
+     */
     fun generateStreamToken(
         uid: String?,
         onSuccess: (String) -> Unit,
-        onFailure: (String) -> Unit
+        onFailure: (Exception) -> Unit
     ) {
       if (uid.isNullOrBlank()) {
-        onFailure("user_id cannot be null or blank")
+        Log.d(TAG, "User ID is null or blank.")
+        onFailure(Exception("User ID is null or blank."))
         return
       }
       val algorithm = Algorithm.HMAC256(BuildConfig.STREAM_SDK_SECRET)
