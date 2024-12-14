4ce2026947adbdca465870ab9587fd996a5e2491
francelu <france.lu@epfl.ch>
2024-12-14 16:14:19 +0100

    test: e2e timer navigate to sign-up

---
+++ app/src/androidTest/java/com/android/periodpals/endtoend/EndToEndTimer.kt
@@ -0,0 +1,64 @@
+package com.android.periodpals.endtoend
+
+import android.Manifest
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onAllNodesWithTag
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performScrollTo
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import androidx.test.rule.GrantPermissionRule
+import com.android.periodpals.MainActivity
+import com.android.periodpals.resources.C.Tag.AuthenticationScreens.SignInScreen
+import com.android.periodpals.resources.C.Tag.AuthenticationScreens.SignUpScreen
+import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+private const val TAG = "EndToEndTimer"
+private const val TIMEOUT = 60000L // 60 seconds, adjust for slower devices, networks and CI
+
+@RunWith(AndroidJUnit4::class)
+class EndToEndTimer : TestCase() {
+  @get:Rule val composeTestRule = createComposeRule()
+  @get:Rule val activityRule = ActivityTestRule(MainActivity::class.java)
+  @get:Rule
+  val permissionRule: GrantPermissionRule =
+      GrantPermissionRule.grant(Manifest.permission.POST_NOTIFICATIONS)
+
+  companion object SignUpData {
+    private val signUpName = ('a'..'z').map { it }.shuffled().subList(0, 8).joinToString("")
+    private val signUpEmail = "$signUpName@example.com"
+    private const val PASSWORD = "iLoveSwent1234!"
+    private val randomNumber = (1..9).random()
+    private val dob = "0$randomNumber/0$randomNumber/200$randomNumber"
+    private val signUpDescription = "Short bio containing my name to identify me: $signUpName"
+  }
+
+  @Before
+  fun setUp() {
+    composeTestRule.setContent { MainActivity() }
+  }
+
+  @Test
+  fun timerEndToEnd() {
+    // Navigate to the sign-up screen
+    composeTestRule.waitForIdle()
+    composeTestRule.onNodeWithTag(SignInScreen.SCREEN).assertIsDisplayed()
+    composeTestRule
+        .onNodeWithTag(SignInScreen.NOT_REGISTERED_NAV_LINK)
+        .performScrollTo()
+        .assertIsDisplayed()
+        .performClick()
+
+    // Register a new user
+    composeTestRule.waitForIdle()
+    assertEquals(
+        1, composeTestRule.onAllNodesWithTag(SignUpScreen.SCREEN).fetchSemanticsNodes().size)
+  }
+}
