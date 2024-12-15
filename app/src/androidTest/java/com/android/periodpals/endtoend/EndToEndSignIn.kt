a70ad96a20759e28c1a5a770ff4f4510b2889ec3
francelu <france.lu@epfl.ch>
2024-12-16 00:25:54 +0100

    test: add log and wait for idle in e2e sign-in

--- app/src/androidTest/java/com/android/periodpals/endtoend/EndToEndSignIn.kt
+++ app/src/androidTest/java/com/android/periodpals/endtoend/EndToEndSignIn.kt
@@ -28,13 +28,13 @@ import io.github.jan.supabase.auth.Auth
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.postgrest.Postgrest
 import io.github.jan.supabase.storage.Storage
-import java.util.concurrent.TimeUnit
 import kotlinx.coroutines.runBlocking
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.concurrent.TimeUnit
 
 private const val TAG = "EndToEndSignIn"
 
@@ -136,6 +136,8 @@ class EndToEndSignIn : TestCase() {
   @Test
   fun test() = run {
     step("User signs in") {
+      composeTestRule.waitForIdle()
+      Log.d(TAG, "User arrives on Sign In Screen")
       composeTestRule
           .onNodeWithTag(AuthenticationScreens.EMAIL_FIELD)
           .performScrollTo()
