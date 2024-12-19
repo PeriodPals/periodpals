8648e6a19ef72e09cbffafa907b986683b5a1fed
Bruno Lazarini Sigg <bruno.lazarinisigg@epfl.ch>
2024-12-20 00:58:02 +0100

    style: ktfmt

--- app/src/main/java/com/android/periodpals/ui/navigation/BottomNavigationMenu.kt
+++ app/src/main/java/com/android/periodpals/ui/navigation/BottomNavigationMenu.kt
@@ -1,6 +1,5 @@
 package com.android.periodpals.ui.navigation
 
-import android.content.Context
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
