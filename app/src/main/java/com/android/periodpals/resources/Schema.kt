c516ae7d35f9610aec4f037fca5391d2bbb647a7
Alonso <alonso.coaguila@epfl.ch>
2024-12-20 00:50:59 +0100

    feat: update `users` table in `Schema.kt`

--- app/src/main/java/com/android/periodpals/resources/Schema.kt
+++ app/src/main/java/com/android/periodpals/resources/Schema.kt
@@ -7,16 +7,18 @@ import com.powersync.db.schema.Table
 const val USERS = "users"
 
 val users =
-    Table(
-        USERS,
-        listOf(
-            Column.text("user_id"),
-            Column.text("name"),
-            Column.text("email"),
-            Column.text("imageUrl"),
-            Column.text("description"),
-            Column.text("dob"),
-            Column.text("fcm_token"),
-            Column.text("locationGIS")))
+  Table(
+    USERS,
+    listOf(
+      Column.text("user_id"),
+      Column.text("name"),
+      Column.text("email"),
+      Column.text("imageUrl"),
+      Column.text("description"),
+      Column.text("dob"),
+      Column.integer("preferred_distance"),
+      Column.text("fcm_token")
+    )
+  )
 
 val localSchema: Schema = Schema(listOf(users))
