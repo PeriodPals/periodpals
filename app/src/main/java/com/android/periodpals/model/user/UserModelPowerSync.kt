ffad65e116479dda59d017aba742b17f359115e0
Alonso <alonso.coaguila@epfl.ch>
2024-12-20 02:07:19 +0100

    feat: update `UserModelPowerSync` to the current db topography

--- app/src/main/java/com/android/periodpals/model/user/UserModelPowerSync.kt
+++ app/src/main/java/com/android/periodpals/model/user/UserModelPowerSync.kt
@@ -1,7 +1,6 @@
 package com.android.periodpals.model.user
 
 import android.util.Log
-import com.android.periodpals.model.location.LocationGIS
 import com.powersync.PowerSyncDatabase
 import com.powersync.connector.supabase.SupabaseConnector
 import io.github.jan.supabase.SupabaseClient
@@ -9,8 +8,6 @@ import io.github.jan.supabase.auth.auth
 import io.github.jan.supabase.storage.storage
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
 
 /**
  * Implementation of UserRepository using PowerSync with Supabase.
@@ -23,9 +20,9 @@ private const val TAG = "UserModelPowerSync"
 private const val USERS = "users"
 
 class UserModelPowerSync(
-    private val db: PowerSyncDatabase,
-    private val connector: SupabaseConnector,
-    private val supabase: SupabaseClient
+  private val db: PowerSyncDatabase,
+  private val connector: SupabaseConnector,
+  private val supabase: SupabaseClient
 ) : UserRepository {
 
   private suspend fun sync() {
@@ -38,28 +35,26 @@ class UserModelPowerSync(
   }
 
   override suspend fun loadUserProfile(
-      onSuccess: (UserDto) -> Unit,
-      onFailure: (Exception) -> Unit
+    idUser: String,
+    onSuccess: (UserDto) -> Unit,
+    onFailure: (Exception) -> Unit
   ) {
     try {
       sync()
-      val currUser =
-          supabase.auth.currentUserOrNull()?.id
-              ?: throw Exception("Supabase does not have a user logged in")
-
       val user: UserDto =
-          db.get(
-              "SELECT name, imageUrl, description, dob, preferred_distance, fcm_token, locationGIS FROM $USERS WHERE user_id = ?",
-              listOf(currUser)) {
-                UserDto(
-                    name = it.getString(0)!!,
-                    imageUrl = it.getString(1)!!,
-                    description = it.getString(2)!!,
-                    dob = it.getString(3)!!,
-                    preferred_distance = it.getLong(4)!!.toInt(),
-                    fcm_token = it.getString(5),
-                    locationGIS = Json.decodeFromString<LocationGIS>(it.getString(5)!!))
-              }
+        db.get(
+          "SELECT name, imageUrl, description, dob, preferred_distance, fcm_token, locationGIS FROM $USERS WHERE user_id = ?",
+          listOf(idUser)
+        ) {
+          UserDto(
+            name = it.getString(0)!!,
+            imageUrl = it.getString(1)!!,
+            description = it.getString(2)!!,
+            dob = it.getString(3)!!,
+            preferred_distance = it.getLong(4)!!.toInt(),
+            fcm_token = it.getString(5)
+          )
+        }
 
       Log.d(TAG, "loadUserProfile: Success")
       onSuccess(user)
@@ -69,16 +64,47 @@ class UserModelPowerSync(
     }
   }
 
+  override suspend fun loadUserProfiles(
+    onSuccess: (List<UserDto>) -> Unit,
+    onFailure: (Exception) -> Unit
+  ) {
+    try {
+      sync()
+      val users: List<UserDto> =
+        db.getAll(
+          "SELECT name, imageUrl, description, dob, preferred_distance, fcm_token, locationGIS FROM $USERS",
+          listOf()
+        ) {
+          UserDto(
+            name = it.getString(0)!!,
+            imageUrl = it.getString(1)!!,
+            description = it.getString(2)!!,
+            dob = it.getString(3)!!,
+            preferred_distance = it.getLong(4)!!.toInt(),
+            fcm_token = it.getString(5)
+          )
+        }
+
+      Log.d(TAG, "loadUserProfiles: Success")
+      onSuccess(users)
+    } catch (e: Exception) {
+      Log.d(TAG, "loadUserProfiles: fail to load users profiles: ${e.message}")
+      onFailure(e)
+    }
+
+  }
+
   override suspend fun createUserProfile(
-      user: User,
-      onSuccess: () -> Unit,
-      onFailure: (Exception) -> Unit
+    user: User,
+    onSuccess: () -> Unit,
+    onFailure: (Exception) -> Unit
   ) {
     try {
       db.writeTransaction { tx ->
         tx.execute(
-            "INSERT INTO $USERS (name, imageUrl, description, dob, fcm_token, locationGIS) VALUES (?, ?, ?, ?, ?, ?);",
-            user.asList())
+          "INSERT INTO $USERS (name, imageUrl, description, dob, preferred_distance, fcm_token) VALUES (?, ?, ?, ?, ?, ?);",
+          user.asList()
+        )
       }
       Log.d(TAG, "createUserProfile: Success")
       onSuccess()
@@ -90,35 +116,37 @@ class UserModelPowerSync(
   }
 
   override suspend fun upsertUserProfile(
-      userDto: UserDto,
-      onSuccess: (UserDto) -> Unit,
-      onFailure: (Exception) -> Unit
+    userDto: UserDto,
+    onSuccess: (UserDto) -> Unit,
+    onFailure: (Exception) -> Unit
   ) {
     try {
       val currUser: String? = supabase.auth.currentUserOrNull()?.id
 
       db.writeTransaction { tx ->
         tx.execute(
-            """
-                        INSERT INTO $USERS (name, imageUrl, description, dob, fcm_token, locationGIS)
-                        VALUES (?, ?, ?, ?, ?)
+          """
+                        INSERT INTO $USERS (user_id, name, imageUrl, description, dob, preferred_distance, fcm_token)
+                        VALUES (?, ?, ?, ?, ?, ?, ?)
                         ON CONFLICT (user_id)
-                        DO UPDATE SET name = ?, imageUrl = ?, description = ?, dob = ?, fcm_token = ?, locationGIS = ?;
+                        DO UPDATE SET name = ?, imageUrl = ?, description = ?, dob = ?, preferred_distance = ?, fcm_token = ?;
                     """,
-            listOf(
-                currUser,
-                userDto.name,
-                userDto.imageUrl,
-                userDto.description,
-                userDto.dob,
-                userDto.fcm_token,
-                Json.encodeToString(userDto.locationGIS),
-                userDto.name,
-                userDto.imageUrl,
-                userDto.description,
-                userDto.dob,
-                userDto.fcm_token,
-                Json.encodeToString(userDto.locationGIS)))
+          listOf(
+            currUser,
+            userDto.name,
+            userDto.imageUrl,
+            userDto.description,
+            userDto.dob,
+            userDto.preferred_distance,
+            userDto.fcm_token,
+            userDto.name,
+            userDto.imageUrl,
+            userDto.description,
+            userDto.dob,
+            userDto.preferred_distance,
+            userDto.fcm_token
+          )
+        )
       }
       Log.d(TAG, "upsertUserProfile: Success")
       sync()
@@ -130,14 +158,14 @@ class UserModelPowerSync(
   }
 
   override suspend fun deleteUserProfile(
-      idUser: String,
-      onSuccess: () -> Unit,
-      onFailure: (Exception) -> Unit
+    idUser: String,
+    onSuccess: () -> Unit,
+    onFailure: (Exception) -> Unit
   ) {
     try {
       val currUser =
-          supabase.auth.currentUserOrNull()?.id
-              ?: throw Exception("Supabase does not have a user logged in")
+        supabase.auth.currentUserOrNull()?.id
+          ?: throw Exception("Supabase does not have a user logged in")
 
       db.writeTransaction { tx ->
         tx.execute("DELETE FROM $USERS WHERE user_id = ?", listOf(currUser))
@@ -152,10 +180,10 @@ class UserModelPowerSync(
   }
 
   override suspend fun uploadFile(
-      filePath: String,
-      bytes: ByteArray,
-      onSuccess: () -> Unit,
-      onFailure: (Exception) -> Unit,
+    filePath: String,
+    bytes: ByteArray,
+    onSuccess: () -> Unit,
+    onFailure: (Exception) -> Unit,
   ) {
     try {
       withContext(Dispatchers.Main) {
@@ -170,9 +198,9 @@ class UserModelPowerSync(
   }
 
   override suspend fun downloadFile(
-      filePath: String,
-      onSuccess: (bytes: ByteArray) -> Unit,
-      onFailure: (Exception) -> Unit,
+    filePath: String,
+    onSuccess: (bytes: ByteArray) -> Unit,
+    onFailure: (Exception) -> Unit,
   ) {
     try {
       withContext(Dispatchers.Main) {
